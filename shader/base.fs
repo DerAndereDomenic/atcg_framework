8d680efff388f757e99789baed15a25f5938e3f9
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-09-02 16:38:40 +0200

    Add some nice shading to objects

--- shader/base.fs
+++ shader/base.fs
@@ -3,10 +3,18 @@
 layout (location = 0) out vec4 outColor;
 
 in vec3 frag_normal;
+in vec3 frag_pos;
 
 uniform vec3 camera_pos;
 
 void main()
 {
-    outColor = vec4(frag_normal,1);
+    vec3 ambient = vec3(0.1f);
+    vec3 light_radiance = vec3(2.0);
+    float light_distance = 1.0f;
+    vec3 light_dir = normalize(camera_pos);
+
+    vec3 color = vec3(1) / 3.14159f * light_radiance * max(0.0f, dot(frag_normal, light_dir)) + ambient;
+
+    outColor = vec4(pow(vec3(1) - exp(-color), vec3(1.0/2.4)),1);
 }
--- shader/base.vs
+++ shader/base.vs
@@ -6,9 +6,11 @@ layout (location = 1) in vec3 aNormal;
 uniform mat4 M, V, P;
 
 out vec3 frag_normal;
+out vec3 frag_pos;
 
 void main()
 {
     gl_Position = P * V * M * vec4(aPosition, 1);
+    frag_pos = vec3(M * vec4(aPosition, 1));
     frag_normal = aNormal;
 }
