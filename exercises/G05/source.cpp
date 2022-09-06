3a90c82975b9e7ddaec6a654eecc2304b36567aa
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-09-06 17:42:51 +0200

    Add framework for next exercise

---
+++ exercises/G05/source.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+#include <Core/EntryPoint.h>
+#include <ATCG.h>
+
+#include <glad/glad.h>
+
+#include <glfw/glfw3.h>
+#include <imgui.h>
+#include <algorithm>
+#include <queue>
+
+#include <numeric>
+
+
+class G05Layer : public atcg::Layer
+{
+public:
+
+    G05Layer(const std::string& name) : atcg::Layer(name) {}
+
+    // This is run at the start of the program
+    virtual void onAttach() override
+    {
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        mesh = std::make_shared<atcg::Mesh>();
+        OpenMesh::IO::read_mesh(*mesh.get(), "res/suzanne_blender.obj");
+
+
+        mesh->uploadData();
+    }
+
+    // This gets called each frame
+    virtual void onUpdate(float delta_time) override
+    {
+        camera_controller->onUpdate(delta_time);
+
+        atcg::Renderer::clear();
+
+        if(mesh && render_faces)
+            atcg::Renderer::draw(mesh, atcg::ShaderManager::getShader("base"), camera_controller->getCamera());
+
+        if(mesh && render_points)
+            atcg::Renderer::drawPoints(mesh, glm::vec3(0), atcg::ShaderManager::getShader("flat"), camera_controller->getCamera());
+
+        if(mesh && render_edges)
+            atcg::Renderer::drawLines(mesh, glm::vec3(0), camera_controller->getCamera());
+    }
+
+    virtual void onImGuiRender() override
+    {
+        ImGui::BeginMainMenuBar();
+
+        if(ImGui::BeginMenu("Rendering"))
+        {
+            ImGui::MenuItem("Show Render Settings", nullptr, &show_render_settings);
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+
+        if(show_render_settings)
+        {
+            ImGui::Begin("Settings", &show_render_settings);
+
+            ImGui::Checkbox("Render Vertices", &render_points);
+            ImGui::Checkbox("Render Edges", &render_edges);
+            ImGui::Checkbox("Render Mesh", &render_faces);
+            ImGui::End();
+        }
+
+
+    }
+
+    // This function is evaluated if an event (key, mouse, resize events, etc.) are triggered
+    virtual void onEvent(atcg::Event& event) override
+    {
+        camera_controller->onEvent(event);
+
+        atcg::EventDispatcher dispatcher(event);
+        dispatcher.dispatch<atcg::FileDroppedEvent>(ATCG_BIND_EVENT_FN(G05Layer::onFileDropped));
+    }
+
+    bool onFileDropped(atcg::FileDroppedEvent& event)
+    {
+        mesh = std::make_shared<atcg::Mesh>();
+        OpenMesh::IO::read_mesh(*mesh.get(), event.getPath());
+
+        mesh->uploadData();
+
+        //Also reset camera
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        return true;
+    }
+
+private:
+    std::shared_ptr<atcg::CameraController> camera_controller;
+    std::shared_ptr<atcg::Mesh> mesh;
+
+    bool show_render_settings = false;
+    bool render_faces = true;
+    bool render_points = false;
+    bool render_edges = false;
+};
+
+class G05 : public atcg::Application
+{
+    public:
+
+    G05()
+        :atcg::Application()
+    {
+        pushLayer(new G05Layer("Layer"));
+    }
+
+    ~G05() {}
+
+};
+
+atcg::Application* atcg::createApplication()
+{
+    return new G05;
+}
