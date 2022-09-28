0bf6d790471ad8f3f93fc6f9ffd0490902cb6532
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-09-28 11:04:16 +0200

    Shade flat vertex colors

--- shader/flat.fs
+++ shader/flat.fs
@@ -2,9 +2,11 @@
 
 layout (location = 0) out vec4 fragColor;
 
+in vec3 frag_color;
+
 uniform vec3 flat_color;
 
 void main()
 {
-    fragColor = vec4(flat_color, 1);
+    fragColor = vec4(frag_color * flat_color, 1);
 }
