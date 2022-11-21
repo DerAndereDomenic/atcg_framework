3a85a92541917d99304a0e2f25dbf3fd43bff005
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-11-21 15:09:28 +0100

    Add docs

--- atcg_lib/include/DataStructure/Statistics.h
+++ atcg_lib/include/DataStructure/Statistics.h
@@ -5,20 +5,53 @@
 
 namespace atcg
 {
+    /**
+     * @brief A class to model a statistic 
+     * @tparam The type of data
+     */
     template<typename T>
     class Statistic
     {
     public:
+        /**
+         * @brief Create a new statistic.
+         * 
+         * @param name The name of the statistic
+         */
         Statistic(const std::string& name) :_name(name) {}
 
+        /**
+         * @brief Destructor 
+         */
         ~Statistic() = default;
 
+        /**
+         * @brief Add a sample to the statistic 
+         * 
+         * @param sample The new sample
+         */
         void addSample(const T& sample);
 
+        /**
+         * @brief Get the mean of the data points
+         * 
+         * @return Estimate of the mean
+         */
         T mean() const;
 
+        /**
+         * @brief Get the (unbiased) variance of the data 
+         * 
+         * @return The variance
+         */
         T var() const;
 
+        /**
+         * @brief Prints the mean and standard deviation of the underlying data 
+         * 
+         * @param os The ostream
+         * @return The ostream
+         */
         std::ostream& operator<<(std::ostream& os);
     private:
         std::vector<T> _samples;
@@ -63,7 +96,7 @@ namespace atcg
     {
         os << "Statistic for " << _name << ":\t";
         os << mean() << "\t";
-        os << "( " << var() << " )\n";
+        os << "( " << std::sqrt(var()) << " )\n";
         return os;
     }
 }
