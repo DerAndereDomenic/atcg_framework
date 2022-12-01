ce4d037444f4c3d378a897f9616d4e24ceabda37
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-12-01 10:47:02 +0100

    Add cotan laplace

--- exercises/G06/source.cpp
+++ exercises/G06/source.cpp
@@ -16,8 +16,25 @@ using VertexHandle = atcg::Mesh::VertexHandle;
 using EdgeHandle = atcg::Mesh::EdgeHandle;
 
 template<typename T>
-struct LaplaceUniform
+struct LaplaceCotan
 {
+    T clampCotan(T v)
+    {
+        const T bound = T(19.1);
+        return (v < -bound ? -bound : (v > bound ? bound : v));
+    }
+
+    T triangleCotan(const atcg::TriMesh::Point& v0, const atcg::TriMesh::Point& v1, const atcg::TriMesh::Point& v2)
+    {
+        const auto d0 = v0 - v2;
+        const auto d1 = v1 - v2;
+        const auto d2 = v1 - v0;
+        const auto area = atcg::areaFromMetric<T>(d0.norm(), d1.norm(), d2.norm());
+        if(area > 1e-5)
+            return clampCotan(d0.dot(d1) / area);
+        return T(1e-5);
+    }
+
     atcg::Laplacian<T> calculate(const std::shared_ptr<atcg::Mesh>& mesh)
     {
         std::vector<Eigen::Triplet<T>> edge_weights;
@@ -27,17 +44,58 @@ struct LaplaceUniform
             uint32_t i = e_it->v0().idx();
             uint32_t j = e_it->v1().idx();
 
-            edge_weights.emplace_back(i, j, T(1.0));
-            edge_weights.emplace_back(j, i, T(1.0));
-            edge_weights.emplace_back(i, i, T(-1.0));
-            edge_weights.emplace_back(j, j, T(-1.0));
+            const auto h0 = e_it->h0();
+            const auto h1 = e_it->h1();
+
+            const auto p0 = h0.to();
+            const auto p1 = h1.to();
+
+            T weight = 0;
+            if(!mesh->is_boundary(h0))
+            {
+                const auto p2 = h0.next().to();
+                weight += triangleCotan(mesh->point(p0), mesh->point(p1), mesh->point(p2));
+            }
+
+            if(!mesh->is_boundary(h1))
+            {
+                const auto p2 = h1.next().to();
+                weight += triangleCotan(mesh->point(p0), mesh->point(p1), mesh->point(p2));
+            }
+
+            edge_weights.emplace_back(i, j, weight);
+            edge_weights.emplace_back(j, i, weight);
+            edge_weights.emplace_back(i, i, -weight);
+            edge_weights.emplace_back(j, j, -weight);
         }
 
         std::vector<Eigen::Triplet<T>> vertex_weights;
 
         for(auto v_it = mesh->vertices_begin(); v_it != mesh->vertices_end(); ++v_it)
         {
-            vertex_weights.emplace_back(v_it->idx(), v_it->idx(), T(v_it->valence()));
+            T weight = 0;
+
+            for(const auto& e : v_it->edges())
+            {
+                const auto h0 = e.h0();
+                const auto h1 = e.h1();
+
+                const auto p0 = h0.to();
+                const auto p1 = h1.to();
+
+                if(!mesh->is_boundary(h0))
+                {
+                    const auto p2 = h0.next().to();
+                    weight += triangleCotan(mesh->point(p0), mesh->point(p1), mesh->point(p2));
+                }
+
+                if(!mesh->is_boundary(h1))
+                {
+                    const auto p2 = h1.next().to();
+                    weight += triangleCotan(mesh->point(p0), mesh->point(p1), mesh->point(p2));
+                }
+            }
+            vertex_weights.emplace_back(v_it->idx(), v_it->idx(), weight);
         }
 
         size_t N = mesh->n_vertices();
@@ -114,7 +172,7 @@ public:
             }
         }
 
-        laplacian = LaplaceUniform<double>().calculate(mesh);
+        laplacian = LaplaceCotan<double>().calculate(mesh);
 
         Eigen::SparseMatrix<double> L = laplacian.M.cwiseInverse() * laplacian.S;
 
