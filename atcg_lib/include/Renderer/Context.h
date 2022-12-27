9e1f7441f0cb88a37877cec36c6a0b7a02dd71d2
Domenic Zingsheim <domeniczingsheim@gmail.com>
2022-12-27 11:44:11 +0100

    Add Graphics context class

---
+++ atcg_lib/include/Renderer/Context.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace atcg
+{
+    /**
+    *   @brief A class to model a graphcis context
+    */
+    class Context
+    {
+    public:
+
+        /**
+        *   @brief Constructor
+        */
+        Context() = default;
+
+        /**
+        *   @brief Initiliaze the context
+        *   @param window The window for the context
+        */
+        void init(void* window);
+
+        /**
+        *   @brief Swap buffers in the swap chain
+        */
+        void swapBuffers();
+
+    private:
+        void* _window;
+    };
+}
---
+++ atcg_lib/src/Renderer/Context.cpp
@@ -0,0 +1,28 @@
+#include <Renderer/Context.h>
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace atcg
+{
+    void Context::init(void* window)
+    {
+        if (!gladLoadGL())
+        {
+            std::cerr << "Error loading glad!\n";
+        }
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        _window = window;
+    }
+
+    void Context::swapBuffers()
+    {
+        glfwSwapBuffers((GLFWwindow*)_window);
+    }
+}
