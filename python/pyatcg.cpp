80e7ed12ea9725acda2f4575e7c33df14f1b2b08
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-02-16 17:43:30 +0100

    Add trampoline class

--- python/pyatcg.cpp
+++ python/pyatcg.cpp
@@ -1,28 +1,26 @@
+#define PYBIND11_DETAILED_ERROR_MESSAGES
 #include <pybind11/pybind11.h>
 #include <ATCG.h>
 
 #define STRINGIFY(x)       #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
+
 class PythonLayer : public atcg::Layer
 {
 public:
-    PythonLayer(const std::string& name) : atcg::Layer(name) {}
+    PythonLayer(const std::string& name = "Layer") : atcg::Layer(name) {}
 
     // This is run at the start of the program
-    virtual void onAttach() override {}
+    virtual void onAttach() override { PYBIND11_OVERRIDE(void, atcg::Layer, onAttach); }
 
     // This gets called each frame
-    virtual void onUpdate(float delta_time) override
-    {
-        // Renders points and curves
-        atcg::Renderer::clear();
-    }
+    virtual void onUpdate(float delta_time) override { PYBIND11_OVERRIDE(void, atcg::Layer, onUpdate, delta_time); }
 
-    virtual void onImGuiRender() override {}
+    virtual void onImGuiRender() override { PYBIND11_OVERRIDE(void, atcg::Layer, onImGuiRender); }
 
     // This function is evaluated if an event (key, mouse, resize events, etc.) are triggered
-    virtual void onEvent(atcg::Event& event) override {}
+    virtual void onEvent(atcg::Event* event) override { PYBIND11_OVERRIDE(void, atcg::Layer, onEvent, event); }
 
 private:
 };
@@ -30,7 +28,7 @@ private:
 class PythonApplication : public atcg::Application
 {
 public:
-    PythonApplication() : atcg::Application() { pushLayer(new PythonLayer("Layer")); }
+    PythonApplication() : atcg::Application() {}
 
     ~PythonApplication() {}
 };
@@ -40,9 +38,10 @@ atcg::Application* atcg::createApplication()
     return new PythonApplication;
 }
 
-int entry_point()
+int entry_point(atcg::Layer* layer)
 {
     atcg::Application* app = atcg::createApplication();
+    app->pushLayer(layer);
     app->run();
 
     delete app;
@@ -62,7 +61,17 @@ PYBIND11_MODULE(pyatcg, m)
            :toctree: _generate
     )pbdoc";
 
-    m.def("start", &entry_point, "Start the application");
+    m.def("start", &entry_point, "Start the application.");
+
+    py::class_<atcg::Layer, PythonLayer>(m, "Layer")
+        .def(py::init<>())
+        .def("onAttach", &atcg::Layer::onAttach)
+        .def("onUpdate", &atcg::Layer::onUpdate)
+        .def("onImGuiRender", &atcg::Layer::onImGuiRender)
+        .def("onEvent", &atcg::Layer::onEvent);
+
+    py::class_<atcg::Event>(m, "Event");
+    py::class_<atcg::MouseMovedEvent, atcg::Event>(m, "MouseMovedEvent").def(py::init<const float, const float>());
 
 
 #ifdef VERSION_INFO
