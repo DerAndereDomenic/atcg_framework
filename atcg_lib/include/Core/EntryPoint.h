4ac40b761b6b8dcbf724f2103de611f91f564135
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-05-25 17:01:55 +0200

    Print memory statistics at the end of the application

--- atcg_lib/include/Core/EntryPoint.h
+++ atcg_lib/include/Core/EntryPoint.h
@@ -14,5 +14,23 @@ int main(int argc, char** argv)
     atcg::ShaderManager::destroy();
     atcg::Renderer::destroy();
 
+    atcg::host_allocator alloc_host;
+    std::size_t host_bytes_allocated   = alloc_host.bytes_allocated;
+    std::size_t host_bytes_deallocated = alloc_host.bytes_deallocated;
+
+    atcg::device_allocator alloc_dev;
+    std::size_t dev_bytes_allocated   = alloc_dev.bytes_allocated;
+    std::size_t dev_bytes_deallocated = alloc_dev.bytes_deallocated;
+
+    ATCG_INFO("Memory statistics: Bytes freed/Bytes allocated (Bytes leaked)");
+    ATCG_INFO("Host: {0}/{1} ({2})",
+              host_bytes_deallocated,
+              host_bytes_allocated,
+              host_bytes_allocated - host_bytes_deallocated);
+    ATCG_INFO("Device: {0}/{1} ({2})",
+              dev_bytes_deallocated,
+              dev_bytes_allocated,
+              dev_bytes_allocated - dev_bytes_deallocated);
+
     return 0;
 }
