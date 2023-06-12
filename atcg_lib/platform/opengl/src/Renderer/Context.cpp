aa31b68ad7e793d7e5d61cc52138f21e030646e8
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-06-12 09:56:47 +0200

    Add opengl debug output in debug mode

--- atcg_lib/platform/opengl/src/Renderer/Context.cpp
+++ atcg_lib/platform/opengl/src/Renderer/Context.cpp
@@ -5,8 +5,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <Core/Log.h>
+
 namespace atcg
 {
+
+namespace detail
+{
+void GLAPIENTRY MessageCallback(GLenum source,
+                                GLenum type,
+                                GLuint id,
+                                GLenum severity,
+                                GLsizei length,
+                                const GLchar* message,
+                                const void* userParam)
+{
+    switch(severity)
+    {
+        case GL_DEBUG_SEVERITY_LOW:
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        {
+            if(id == 131218) return;    // Some NVIDIA stuff going wrong -> disable this warning
+            ATCG_WARN(message);
+        }
+        break;
+        case GL_DEBUG_SEVERITY_HIGH:
+        {
+            ATCG_ERROR(message);
+        }
+        break;
+        default:
+            break;
+    }
+}
+}    // namespace detail
+
 void Context::init(void* window)
 {
     glfwMakeContextCurrent((GLFWwindow*)window);
@@ -14,6 +47,11 @@ void Context::init(void* window)
     if(!gladLoadGL()) { std::cerr << "Error loading glad!\n"; }
 
     _window = window;
+
+#ifndef NDEBUG
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(detail::MessageCallback, 0);
+#endif
 }
 
 void Context::swapBuffers()
