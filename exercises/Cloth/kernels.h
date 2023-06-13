81c6157ff019a8c5c1dcfabd987ba9b56112c4c3
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-06-13 15:32:35 +0200

    Add some dummy code so github actions do not complain

--- exercises/Cloth/kernels.h
+++ exercises/Cloth/kernels.h
@@ -3,4 +3,9 @@
 #include <Core/Memory.h>
 #include <Core/glm.h>
 
+#ifdef ATCG_CUDA_BACKEND
 void simulate(glm::vec3* points, uint32_t size, float time);
+#else
+// Just some hackery so that the github actions run through if CUDA is not used
+void simulate(glm::vec3* points, uint32_t size, float time) {}
+#endif
