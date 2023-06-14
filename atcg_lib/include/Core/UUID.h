225bc10590c7e8c572c7a57336f9d7500b538ebe
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-06-14 17:11:27 +0200

    Add hash operation for uuid

--- atcg_lib/include/Core/UUID.h
+++ atcg_lib/include/Core/UUID.h
@@ -37,3 +37,15 @@ private:
     uint64_t _UUID;
 }
 }    // namespace atcg
+
+namespace std
+{
+template<typename T>
+struct hash;
+
+template<>
+struct hash<atcg::UUID>
+{
+    size_t operator()(const atcg::UUID& uuid) const { return (uint64_t)uuid; }
+}
+}    // namespace std
