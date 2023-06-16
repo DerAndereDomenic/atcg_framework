c65dd94150ed36c4f2b4069be5aaf091064155c3
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-06-16 09:50:52 +0200

    Add ID component to every entity

--- atcg_lib/src/Scene/Scene.cpp
+++ atcg_lib/src/Scene/Scene.cpp
@@ -1,12 +1,14 @@
 #include <Scene/Scene.h>
 
 #include <Scene/Entity.h>
+#include <Scene/Components.h>
 
 namespace atcg
 {
 Entity Scene::createEntity()
 {
     Entity entity(_registry.create(), this);
+    entity.addComponent<IDComponent>();
     return entity;
 }
 }    // namespace atcg
