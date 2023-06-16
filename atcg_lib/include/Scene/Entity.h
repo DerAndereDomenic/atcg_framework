7b8cd230811bdd010fa08c4a53c8e37ffdb5c752
Domenic Zingsheim <domeniczingsheim@gmail.com>
2023-06-16 09:19:51 +0200

    Add Entity class

---
+++ atcg_lib/include/Scene/Entity.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <entt.hpp>
+
+#include <Scene/Scene.h>
+
+namespace atcg
+{
+class Entity
+{
+public:
+    Entity() = default;
+
+    template<typename T, typename... Args>
+    T& addComponent(Args&&... args)
+    {
+        T& component = _scene->_registry.emplace<T>(_entity_handle, std::forward<Args>(args)...);
+        return component;
+    }
+
+    template<typename T>
+    T& getComponent()
+    {
+        return _scene->registry.get<T>(_entity_handle);
+    }
+
+    template<typename T>
+    bool hasComponent()
+    {
+        return _scene->_regsitry.has<T>(_entity_handle);
+    }
+
+    operator() bool() const { return _entity_handle != entt::null; }
+
+private:
+    Entity(entt::entity handle, Scene* scene);
+
+    Scene* _scene               = nullptr;
+    entt::entity _entity_handle = entt::null;
+};
+}    // namespace atcg
