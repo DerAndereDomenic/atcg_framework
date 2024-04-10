d952d13b6c1d45db7342d7eb5afa218234e6a708
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-04-10 16:49:13 +0200

    Move definition of surface interaction into own file to remove torch from optix files

---
+++ atcg_lib/include/Math/SurfaceInteraciton.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace atcg
+{
+
+struct BSDFVPtrTable;
+
+struct SurfaceInteraction
+{
+    bool valid = false;
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 barys;
+    glm::vec2 uv;
+    glm::vec3 incoming_direction;
+    float incoming_distance;
+    uint32_t primitive_idx;
+
+    const BSDFVPtrTable* bsdf;
+};
+}    // namespace atcg
--- atcg_lib/include/Math/Tracing.h
+++ atcg_lib/include/Math/Tracing.h
@@ -2,22 +2,13 @@
 
 #include <nanort.h>
 #include <torch/types.h>
+#include <Math/SurfaceInteraciton.h>
+
 
 namespace atcg
 {
 
 class Entity;
-struct SurfaceInteraction
-{
-    bool valid = false;
-    glm::vec3 position;
-    glm::vec3 normal;
-    glm::vec2 barys;
-    glm::vec2 uv;
-    glm::vec3 incoming_direction;
-    float incoming_distance;
-    uint32_t primitive_idx;
-};
 
 namespace Tracing
 {
