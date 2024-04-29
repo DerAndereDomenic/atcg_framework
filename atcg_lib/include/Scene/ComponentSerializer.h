4bf198b85cc7c280e01adff29e6dbe0421da77bc
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-04-29 21:07:53 +0200

    Add deserialization function

--- atcg_lib/include/Scene/ComponentSerializer.h
+++ atcg_lib/include/Scene/ComponentSerializer.h
@@ -15,9 +15,14 @@ public:
     template<typename T>
     void serialize_component(const std::string& file_path, Entity entity, T& component, nlohmann::json& j);
 
+    template<typename T>
+    void deserialize_component(const std::string& file_path, Entity entity, nlohmann::json& j);
+
 protected:
     void serializeBuffer(const std::string& file_name, const char* data, const uint32_t byte_size);
+    std::vector<uint8_t> deserializeBuffer(const std::string& file_name);
     void serializeMaterial(nlohmann::json& out, Entity entity, const Material& material, const std::string& file_path);
+    Material deserialize_material(const nlohmann::json& material_node);
     void serializeTexture(const atcg::ref_ptr<Texture2D>& texture, std::string& path, float gamma = 1.0f);
     atcg::ref_ptr<Scene> _scene;
 };
--- atcg_lib/src/Scene/ComponentSerializer.cpp
+++ atcg_lib/src/Scene/ComponentSerializer.cpp
@@ -2,6 +2,8 @@
 
 #include <Scene/Entity.h>
 
+#include <DataStructure/TorchUtils.h>
+
 namespace atcg
 {
 void ComponentSerializer::serializeBuffer(const std::string& file_name, const char* data, const uint32_t byte_size)
@@ -9,6 +11,15 @@ void ComponentSerializer::serializeBuffer(const std::string& file_name, const ch
     std::ofstream summary_file(file_name, std::ios::out | std::ios::binary);
     summary_file.write(data, byte_size);
     summary_file.close();
+}
+
+std::vector<uint8_t> ComponentSerializer::deserializeBuffer(const std::string& file_name)
+{
+    std::ifstream summary_file(file_name, std::ios::in | std::ios::binary);
+    std::vector<uint8_t> buffer_char(std::istreambuf_iterator<char>(summary_file), {});
+    summary_file.close();
+
+    return buffer_char;
 }
 
 void ComponentSerializer::serializeTexture(const atcg::ref_ptr<Texture2D>& texture, std::string& path, float gamma)
@@ -95,7 +106,7 @@ void ComponentSerializer::serializeMaterial(nlohmann::json& j,
     {
         std::string img_path = file_path + "_" + std::to_string(entity_id) + "_roughness";
 
-        serializeTexture(roughness_texture, img_path, 1.0f / 2.2f);
+        serializeTexture(roughness_texture, img_path);
 
         j["Material"]["RoughnessTexture"] = img_path;
     }
@@ -106,6 +117,65 @@ void ComponentSerializer::serializeMaterial(nlohmann::json& j,
 
         j["Material"]["Roughness"] = color;
     }
+}
+
+Material ComponentSerializer::deserialize_material(const nlohmann::json& material_node)
+{
+    Material material;
+
+    // Diffuse
+    if(material_node.contains("Diffuse"))
+    {
+        std::vector<float> diffuse_color = material_node["Diffuse"];
+        material.setDiffuseColor(glm::vec4(glm::make_vec3(diffuse_color.data()), 1.0f));
+    }
+    else if(material_node.contains("DiffuseTexture"))
+    {
+        std::string diffuse_path = material_node["DiffuseTexture"];
+        ATCG_TRACE(diffuse_path);
+        auto img             = IO::imread(diffuse_path, 2.2f);
+        auto diffuse_texture = atcg::Texture2D::create(img);
+        material.setDiffuseTexture(diffuse_texture);
+    }
+
+    // Normals
+    if(material_node.contains("NormalTexture"))
+    {
+        std::string normal_path = material_node["NormalTexture"];
+        auto img                = IO::imread(normal_path);
+        auto normal_texture     = atcg::Texture2D::create(img);
+        material.setNormalTexture(normal_texture);
+    }
+
+    // Roughness
+    if(material_node.contains("Roughness"))
+    {
+        float roughness = material_node["Roughness"];
+        material.setRoughness(roughness);
+    }
+    else if(material_node.contains("RoughnessTexture"))
+    {
+        std::string roughness_path = material_node["RoughnessTexture"];
+        auto img                   = IO::imread(roughness_path);
+        auto roughness_texture     = atcg::Texture2D::create(img);
+        material.setRoughnessTexture(roughness_texture);
+    }
+
+    // Metallic
+    if(material_node.contains("Metallic"))
+    {
+        float metallic = material_node["Metallic"];
+        material.setMetallic(metallic);
+    }
+    else if(material_node.contains("MetallicTexture"))
+    {
+        std::string metallic_path = material_node["MetallicTexture"];
+        auto img                  = IO::imread(metallic_path);
+        auto metallic_texture     = atcg::Texture2D::create(img);
+        material.setMetallicTexture(metallic_texture);
+    }
+
+    return material;
 }
 
 template<typename T>
@@ -258,5 +328,309 @@ void ComponentSerializer::serialize_component<EdgeCylinderRenderComponent>(const
     j["EdgeCylinderRenderer"]["Radius"] = component.radius;
 
     serializeMaterial(j["EdgeCylinderRenderer"], entity, component.material, file_path);
+}
+
+template<typename T>
+void ComponentSerializer::deserialize_component(const std::string& file_path, Entity entity, nlohmann::json& j)
+{
+}
+
+template<>
+void ComponentSerializer::deserialize_component<IDComponent>(const std::string& file_path,
+                                                             Entity entity,
+                                                             nlohmann::json& j)
+{
+    if(!j.contains("ID"))
+    {
+        return;
+    }
+
+    auto& component = entity.getComponent<IDComponent>();
+    component.ID    = (uint64_t)j["ID"];
+}
+
+template<>
+void ComponentSerializer::deserialize_component<NameComponent>(const std::string& file_path,
+                                                               Entity entity,
+                                                               nlohmann::json& j)
+{
+    if(!j.contains("Name"))
+    {
+        return;
+    }
+
+    auto& component = entity.getComponent<NameComponent>();
+    component.name  = j["Name"];
+}
+
+template<>
+void ComponentSerializer::deserialize_component<TransformComponent>(const std::string& file_path,
+                                                                    Entity entity,
+                                                                    nlohmann::json& j)
+{
+    if(!j.contains("Transform"))
+    {
+        return;
+    }
+
+    std::vector<float> position = j["Transform"]["Position"];
+    std::vector<float> scale    = j["Transform"]["Scale"];
+    std::vector<float> rotation = j["Transform"]["EulerAngles"];
+
+    entity.addComponent<atcg::TransformComponent>(glm::make_vec3(position.data()),
+                                                  glm::make_vec3(scale.data()),
+                                                  glm::make_vec3(rotation.data()));
+}
+
+template<>
+void ComponentSerializer::deserialize_component<CameraComponent>(const std::string& file_path,
+                                                                 Entity entity,
+                                                                 nlohmann::json& j)
+{
+    if(!j.contains("PerspectiveCamera"))
+    {
+        return;
+    }
+
+    float aspect_ratio = j["PerspectiveCamera"]["AspectRatio"];
+    float fov          = j["PerspectiveCamera"]["FoVy"];
+    auto& camera       = entity.addComponent<CameraComponent>(
+        atcg::make_ref<atcg::PerspectiveCamera>(aspect_ratio, glm::vec3(0), glm::vec3(0, 0, 1)));
+    atcg::ref_ptr<PerspectiveCamera> cam = camera.camera;
+    // TODO: Keep consistent
+    cam->setFOV(fov);
+    // Should always have a transform, otherwise construct default camera
+    if(entity.hasComponent<TransformComponent>())
+    {
+        cam->setFromTransform(entity.getComponent<TransformComponent>().getModel());
+    }
+}
+
+
+template<>
+void ComponentSerializer::deserialize_component<GeometryComponent>(const std::string& file_path,
+                                                                   Entity entity,
+                                                                   nlohmann::json& j)
+{
+    if(!j.contains("Geometry"))
+    {
+        return;
+    }
+
+    auto& geometry       = entity.addComponent<GeometryComponent>();
+    atcg::GraphType type = (atcg::GraphType)(int)j["Geometry"]["Type"];
+
+    switch(type)
+    {
+        case atcg::GraphType::ATCG_GRAPH_TYPE_TRIANGLEMESH:
+        {
+            std::string vertex_path           = j["Geometry"]["Vertices"];
+            std::string faces_path            = j["Geometry"]["Faces"];
+            std::vector<uint8_t> vertices_raw = deserializeBuffer(vertex_path);
+            std::vector<uint8_t> faces_raw    = deserializeBuffer(faces_path);
+
+            auto vertices = atcg::createHostTensorFromPointer(
+                (float*)vertices_raw.data(),
+                {(int)(vertices_raw.size() / sizeof(Vertex)), atcg::VertexSpecification::VERTEX_SIZE});
+
+            auto faces = atcg::createHostTensorFromPointer((int32_t*)faces_raw.data(),
+                                                           {(int)(faces_raw.size() / sizeof(glm::u32vec3)), 3});
+
+            geometry.graph = Graph::createTriangleMesh(vertices, faces);
+        }
+        break;
+        case atcg::GraphType::ATCG_GRAPH_TYPE_POINTCLOUD:
+        {
+            std::string vertex_path           = j["Geometry"]["Vertices"];
+            std::vector<uint8_t> vertices_raw = deserializeBuffer(vertex_path);
+
+            auto vertices = atcg::createHostTensorFromPointer(
+                (float*)vertices_raw.data(),
+                {(int)(vertices_raw.size() / sizeof(Vertex)), atcg::VertexSpecification::VERTEX_SIZE});
+
+            geometry.graph = Graph::createPointCloud(vertices);
+        }
+        break;
+        case atcg::GraphType::ATCG_GRAPH_TYPE_GRAPH:
+        {
+            std::string vertex_path           = j["Geometry"]["Vertices"];
+            std::string edges_path            = j["Geometry"]["Edges"];
+            std::vector<uint8_t> vertices_raw = deserializeBuffer(vertex_path);
+            std::vector<uint8_t> edges_raw    = deserializeBuffer(edges_path);
+
+            auto vertices = atcg::createHostTensorFromPointer(
+                (float*)vertices_raw.data(),
+                {(int)(vertices_raw.size() / sizeof(Vertex)), atcg::VertexSpecification::VERTEX_SIZE});
+
+            auto edges = atcg::createHostTensorFromPointer(
+                (float*)edges_raw.data(),
+                {(int)(edges_raw.size() / sizeof(Edge)), atcg::EdgeSpecification::EDGE_SIZE});
+
+            geometry.graph = Graph::createGraph(vertices, edges);
+        }
+        break;
+        default:
+        {
+            ATCG_ERROR("Unknown graph type: {0}", (int)type);
+        }
+        break;
+    }
+}
+
+template<>
+void ComponentSerializer::deserialize_component<MeshRenderComponent>(const std::string& file_path,
+                                                                     Entity entity,
+                                                                     nlohmann::json& j)
+{
+    if(!j.contains("MeshRenderer"))
+    {
+        return;
+    }
+
+    auto& renderer            = j["MeshRenderer"];
+    auto& renderComponent     = entity.addComponent<MeshRenderComponent>();
+    std::string vertex_path   = renderer["Shader"]["Vertex"];
+    std::string fragment_path = renderer["Shader"]["Fragment"];
+    std::string geometry_path = renderer["Shader"]["Geometry"];
+
+    std::string shader_name = vertex_path.substr(vertex_path.find_last_of('/') + 1);
+    shader_name             = shader_name.substr(0, shader_name.find_first_of('.'));
+
+    if(ShaderManager::hasShader(shader_name))
+    {
+        renderComponent.shader = ShaderManager::getShader(shader_name);
+    }
+    else if(geometry_path != "")
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path, geometry_path);
+    }
+    else
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path);
+    }
+
+    if(renderer.contains("Material"))
+    {
+        auto material_node       = renderer["Material"];
+        Material material        = deserialize_material(material_node);
+        renderComponent.material = material;
+    }
+}
+
+template<>
+void ComponentSerializer::deserialize_component<PointRenderComponent>(const std::string& file_path,
+                                                                      Entity entity,
+                                                                      nlohmann::json& j)
+{
+    if(!j.contains("PointRenderer"))
+    {
+        return;
+    }
+
+    auto& renderer             = j["PointRenderer"];
+    auto& renderComponent      = entity.addComponent<PointRenderComponent>();
+    std::vector<float> color   = renderer["Color"];
+    renderComponent.color      = glm::make_vec3(color.data());
+    renderComponent.point_size = renderer["PointSize"];
+    std::string vertex_path    = renderer["Shader"]["Vertex"];
+    std::string fragment_path  = renderer["Shader"]["Fragment"];
+    std::string geometry_path  = renderer["Shader"]["Geometry"];
+
+    std::string shader_name = vertex_path.substr(vertex_path.find_last_of('/') + 1);
+    shader_name             = shader_name.substr(0, shader_name.find_first_of('.'));
+
+    if(ShaderManager::hasShader(shader_name))
+    {
+        renderComponent.shader = ShaderManager::getShader(shader_name);
+    }
+    else if(geometry_path != "")
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path, geometry_path);
+    }
+    else
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path);
+    }
+}
+
+template<>
+void ComponentSerializer::deserialize_component<PointSphereRenderComponent>(const std::string& file_path,
+                                                                            Entity entity,
+                                                                            nlohmann::json& j)
+{
+    if(!j.contains("PointSphereRenderer"))
+    {
+        return;
+    }
+
+    auto& renderer             = j["PointSphereRenderer"];
+    auto& renderComponent      = entity.addComponent<PointSphereRenderComponent>();
+    renderComponent.point_size = renderer["PointSize"];
+    std::string vertex_path    = renderer["Shader"]["Vertex"];
+    std::string fragment_path  = renderer["Shader"]["Fragment"];
+    std::string geometry_path  = renderer["Shader"]["Geometry"];
+
+    std::string shader_name = vertex_path.substr(vertex_path.find_last_of('/') + 1);
+    shader_name             = shader_name.substr(0, shader_name.find_first_of('.'));
+
+    if(ShaderManager::hasShader(shader_name))
+    {
+        renderComponent.shader = ShaderManager::getShader(shader_name);
+    }
+    else if(geometry_path != "")
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path, geometry_path);
+    }
+    else
+    {
+        renderComponent.shader = atcg::make_ref<Shader>(vertex_path, fragment_path);
+    }
+
+
+    if(renderer.contains("Material"))
+    {
+        auto material_node       = renderer["Material"];
+        Material material        = deserialize_material(material_node);
+        renderComponent.material = material;
+    }
+}
+
+template<>
+void ComponentSerializer::deserialize_component<EdgeRenderComponent>(const std::string& file_path,
+                                                                     Entity entity,
+                                                                     nlohmann::json& j)
+{
+    if(!j.contains("EdgeRenderer"))
+    {
+        return;
+    }
+
+    auto& renderer           = j["EdgeRenderer"];
+    auto& renderComponent    = entity.addComponent<EdgeRenderComponent>();
+    std::vector<float> color = renderer["Color"];
+    renderComponent.color    = glm::make_vec3(color.data());
+}
+
+template<>
+void ComponentSerializer::deserialize_component<EdgeCylinderRenderComponent>(const std::string& file_path,
+                                                                             Entity entity,
+                                                                             nlohmann::json& j)
+{
+    if(!j.contains("EdgeCylinderRenderer"))
+    {
+        return;
+    }
+
+    auto& renderer         = j["EdgeCylinderRenderer"];
+    auto& renderComponent  = entity.addComponent<EdgeCylinderRenderComponent>();
+    renderComponent.radius = renderer["Radius"];
+
+
+    if(renderer.contains("Material"))
+    {
+        auto& material_node      = renderer["Material"];
+        Material material        = deserialize_material(material_node);
+        renderComponent.material = material;
+    }
 }
 }    // namespace atcg
