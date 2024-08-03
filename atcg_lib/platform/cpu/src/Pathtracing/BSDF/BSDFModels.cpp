848395ef0e76841b0a40aa7b9c3c369e326d6edc
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-08-03 16:09:53 +0200

    Add evaluation of pbr bsdf

--- atcg_lib/platform/cpu/src/Pathtracing/BSDF/BSDFModels.cpp
+++ atcg_lib/platform/cpu/src/Pathtracing/BSDF/BSDFModels.cpp
@@ -73,6 +73,51 @@ BSDFSamplingResult PBRBSDF::sampleBSDF(const SurfaceInteraction& si, PCG32& rng)
 
 BSDFEvalResult PBRBSDF::evalBSDF(const SurfaceInteraction& si, const glm::vec3& outgoing_dir)
 {
-    return BSDFEvalResult();
+    // TODO: Make this more generic
+    atcg::BSDFEvalResult result;
+
+    glm::vec3 diffuse_color = detail::read_image(_diffuse_image, si.uv);
+    float metallic          = detail::read_image(_metallic_image, si.uv).x;
+    float roughness         = detail::read_image(_roughness_image, si.uv).x;
+    roughness = glm::max(roughness * roughness, 1e-3f);    // In the real time shaders, roughness is squared
+
+    glm::vec3 metallic_color = (1.0f - metallic) * glm::vec3(0.04f) + metallic * diffuse_color;
+
+    glm::vec3 light_dir = outgoing_dir;
+    glm::vec3 view_dir  = -si.incoming_direction;
+
+    glm::vec3 H = glm::normalize(light_dir + view_dir);
+
+    float NdotH = glm::max(glm::dot(si.normal, H), 0.0f);
+    float NdotV = glm::max(glm::dot(si.normal, view_dir), 0.0f);
+    float NdotL = glm::max(glm::dot(si.normal, light_dir), 0.0f);
+
+    if(NdotL <= 0.0f || NdotV <= 0.0f) return result;
+
+    float NDF   = atcg::D_GGX(NdotH, roughness);
+    float G     = atcg::geometrySmith(NdotL, NdotV, roughness);
+    glm::vec3 F = atcg::fresnel_schlick(metallic_color, glm::max(glm::dot(H, view_dir), 0.0f));
+
+    glm::vec3 numerator = NDF * G * F;
+    float denominator   = 4.0 * NdotV * NdotL + 1e-5f;
+    glm::vec3 specular  = numerator / denominator;
+
+    glm::vec3 kS = F;
+    glm::vec3 kD = glm::vec3(1.0) - kS;
+    kD *= (1.0 - metallic);
+
+    float diffuse_probability =
+        glm::dot(diffuse_color, glm::vec3(1)) /
+        (glm::dot(diffuse_color, glm::vec3(1)) + glm::dot(metallic_color, glm::vec3(1)) + 1e-5f);
+    float specular_probability    = 1 - diffuse_probability;
+    float diffuse_pdf             = NdotL / glm::pi<float>();
+    float halfway_pdf             = NDF * NdotH;
+    float halfway_to_outgoing_pdf = atcg::warp_normal_to_reflected_direction_pdf(outgoing_dir, H);    // 1 / (4*HdotV)
+    float specular_pdf            = halfway_pdf * halfway_to_outgoing_pdf;
+
+    result.bsdf_value         = specular + kD * diffuse_color / glm::pi<float>();
+    result.sample_probability = diffuse_probability * diffuse_pdf + specular_probability * specular_pdf + 1e-5f;
+
+    return result;
 }
 }    // namespace atcg
