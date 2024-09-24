171322f8f2cc910a23c672660ce2230cc5c1fe93
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-09-24 14:55:22 +0200

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,40 +6,33 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ATCG Lib'
-copyright = '2023, Domenic Zingsheim'
-author = 'Domenic Zingsheim'
+project = "ATCG Lib"
+copyright = "2024, Domenic Zingsheim"
+author = "Domenic Zingsheim"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "breathe",
-    "myst_parser"
-]
+extensions = ["breathe", "myst_parser"]
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # breathe_projects = {
 #     "ATCG Lib": "../bin/doxygen/xml/",
 # }
 
 breathe_default_project = "ATCGLIB"
-breathe_domain_by_extension = {
-    "h": "cpp",
-    "cpp": "cpp"
-}
+breathe_domain_by_extension = {"h": "cpp", "cpp": "cpp"}
 cpp_index_common_prefix = [
-    'atcg::',
+    "atcg::",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
