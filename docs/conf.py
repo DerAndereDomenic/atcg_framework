bd6ea03ff8a49092ba56dee56f070e6c927239b2
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-11-19 15:53:24 +0100

    Separate classes into different files for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -34,5 +34,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
+# html_static_path = ["_static"]
+html_theme_options = {
+    "toc_title": "Navigation",  # Customize the sidebar title
+    "show_toc_level": 1,  # Control depth of sidebar links
+    "collapse_navbar": False,  # Ensure the sidebar doesn't collapse subitems
+}
