39111d51537a453d3aec7e28ae9092ad1a559db1
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-03-07 21:01:27 +0100

    Add RevisionSystem to test suit

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -6,6 +6,7 @@
 #include <Renderer/ShaderManager.h>
 #include <Renderer/Renderer.h>
 #include <Renderer/ContextManager.h>
+#include <Scene/RevisionStack.h>
 
 // Define a custom test environment class
 class ATCGTestEnvironment : public ::testing::Environment
@@ -33,6 +34,9 @@ public:
         _renderer = atcg::make_ref<atcg::RendererSystem>();
         _renderer->init(_window->getWidth(), _window->getHeight(), _window->getContext(), _shader_manager);
         atcg::SystemRegistry::instance()->registerSystem(_renderer.get());
+
+        _revision_system = atcg::make_ref<atcg::RevisionSystem>();
+        atcg::SystemRegistry::instance()->registerSystem(_revision_system.get());
     }
 
     // This will be run once after all tests have finished
@@ -48,6 +52,7 @@ private:
     atcg::ref_ptr<atcg::ShaderManagerSystem> _shader_manager;
     atcg::ref_ptr<atcg::RendererSystem> _renderer;
     atcg::ref_ptr<atcg::Window> _window;
+    atcg::ref_ptr<atcg::RevisionSystem> _revision_system;
 };
 
 int main(int argc, char **argv)
