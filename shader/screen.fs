612a1fab183a0d52aac61a635dcdc5141c394f41
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-05-31 21:27:16 +0200

    Add subroutine to add entity ids from texture when drawing images to the screen

--- shader/screen.fs
+++ shader/screen.fs
@@ -1,4 +1,4 @@
-#version 330 core
+#version 400 core
 
 layout(location = 0) out vec4 FragColor;
 layout(location = 1) out int entityID;
@@ -6,10 +6,26 @@ layout(location = 1) out int entityID;
 in vec2 frag_uv;
 
 uniform sampler2D screen_texture;
+uniform isampler2D entity_ids;
+
+subroutine int _getEntityID();
+
+subroutine(_getEntityID) int getDefaultID()
+{
+    return -1;
+}
+
+subroutine(_getEntityID) int getFromTextureID()
+{
+    return texture(entity_ids, frag_uv).r;
+}
+
+subroutine uniform _getEntityID getEntityID;
 
 void main()
 {
     vec3 color = texture(screen_texture, frag_uv).rgb;
+    int entity_id = getEntityID();
     FragColor = vec4(color, 1);
-    entityID = -1;
+    entityID = entity_id;
 }
