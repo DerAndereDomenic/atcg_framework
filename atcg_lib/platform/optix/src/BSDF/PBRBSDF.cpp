fd9b0bb7e3f313947bed8c4ceb3a2454bb8cc76a
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-06-30 10:35:29 +0200

    Move texture conversion into one method

--- atcg_lib/platform/optix/src/BSDF/PBRBSDF.cpp
+++ atcg_lib/platform/optix/src/BSDF/PBRBSDF.cpp
@@ -1,74 +1,9 @@
 #include <BSDF/PBRBSDF.h>
 
+#include <Core/Common.h>
+
 namespace atcg
 {
-
-namespace detail
-{
-
-ATCG_INLINE static void convertToTextureObject(const torch::Tensor& texture_data,
-                                               cudaArray_t& output_array,
-                                               cudaTextureObject_t& output_texture)
-{
-    torch::Tensor data = texture_data;
-    if(texture_data.size(2) == 2)
-    {
-        data = torch::cat({texture_data, torch::zeros_like(texture_data)}, -1);
-    }
-
-    if(texture_data.size(2) == 3)
-    {
-        data =
-            torch::cat({texture_data,
-                        torch::zeros_like(
-                            texture_data.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}).unsqueeze(-1))},
-                       -1);
-    }
-
-
-    uint32_t num_channels = data.size(2);
-    bool isFloat          = data.dtype() == torch::kFloat32;
-
-    uint32_t component_size = isFloat ? 32 : 8;
-
-    cudaChannelFormatDesc format = {};
-    format.f                     = isFloat ? cudaChannelFormatKindFloat : cudaChannelFormatKindUnsigned;
-    format.x                     = component_size;
-    format.y                     = num_channels == 4 ? component_size : 0;
-    format.z                     = num_channels == 4 ? component_size : 0;
-    format.w                     = num_channels == 4 ? component_size : 0;
-
-    cudaExtent extent = {};
-    extent.width      = data.size(1);
-    extent.height     = data.size(0);
-    extent.depth      = 0;
-
-    CUDA_SAFE_CALL(cudaMalloc3DArray(&output_array, &format, extent));
-
-    CUDA_SAFE_CALL(cudaMemcpy2DToArray(output_array,
-                                       0,
-                                       0,
-                                       data.contiguous().data_ptr(),
-                                       data.size(1) * data.size(2) * data.element_size(),
-                                       data.size(1) * data.size(2) * data.element_size(),
-                                       data.size(0),
-                                       cudaMemcpyDeviceToDevice));
-
-    cudaTextureDesc desc  = {};
-    desc.normalizedCoords = 1;
-    desc.addressMode[0]   = cudaAddressModeClamp;
-    desc.addressMode[1]   = cudaAddressModeClamp;
-    desc.addressMode[2]   = cudaAddressModeClamp;
-    desc.readMode         = isFloat ? cudaReadModeElementType : cudaReadModeNormalizedFloat;
-    desc.filterMode       = cudaFilterModeLinear;
-
-    cudaResourceDesc resDesc = {};
-    resDesc.resType          = cudaResourceTypeArray;
-    resDesc.res.array.array  = output_array;
-
-    CUDA_SAFE_CALL(cudaCreateTextureObject(&output_texture, &resDesc, &desc, nullptr));
-}
-}    // namespace detail
 
 PBRBSDF::PBRBSDF(const Dictionary& dict)
 {
@@ -80,9 +15,9 @@ PBRBSDF::PBRBSDF(const Dictionary& dict)
 
     PBRBSDFData data;
 
-    detail::convertToTextureObject(diffuse_texture, _diffuse_texture, data.diffuse_texture);
-    detail::convertToTextureObject(metallic_texture, _metallic_texture, data.metallic_texture);
-    detail::convertToTextureObject(roughness_texture, _roughness_texture, data.roughness_texture);
+    atcg::convertToTextureObject(diffuse_texture, _diffuse_texture, data.diffuse_texture);
+    atcg::convertToTextureObject(metallic_texture, _metallic_texture, data.metallic_texture);
+    atcg::convertToTextureObject(roughness_texture, _roughness_texture, data.roughness_texture);
 
     _flags = BSDFComponentType::GlossyReflection | BSDFComponentType::DiffuseReflection;
 
--- atcg_lib/platform/optix/src/Emitter/MeshEmitter.cu
+++ atcg_lib/platform/optix/src/Emitter/MeshEmitter.cu
@@ -2,75 +2,13 @@
 #include <ATen/cuda/ApplyGridUtils.cuh>
 #include <c10/cuda/CUDAGuard.h>
 #include <Shape/MeshShape.h>
+#include <Core/Common.h>
 
 namespace atcg
 {
 
 namespace detail
 {
-ATCG_INLINE static void convertToTextureObject(const torch::Tensor& texture_data,
-                                               cudaArray_t& output_array,
-                                               cudaTextureObject_t& output_texture)
-{
-    torch::Tensor data = texture_data;
-    if(texture_data.size(2) == 2)
-    {
-        data = torch::cat({texture_data, torch::zeros_like(texture_data)}, -1);
-    }
-
-    if(texture_data.size(2) == 3)
-    {
-        data =
-            torch::cat({texture_data,
-                        torch::zeros_like(
-                            texture_data.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}).unsqueeze(-1))},
-                       -1);
-    }
-
-
-    uint32_t num_channels = data.size(2);
-    bool isFloat          = data.dtype() == torch::kFloat32;
-
-    uint32_t component_size = isFloat ? 32 : 8;
-
-    cudaChannelFormatDesc format = {};
-    format.f                     = isFloat ? cudaChannelFormatKindFloat : cudaChannelFormatKindUnsigned;
-    format.x                     = component_size;
-    format.y                     = num_channels == 4 ? component_size : 0;
-    format.z                     = num_channels == 4 ? component_size : 0;
-    format.w                     = num_channels == 4 ? component_size : 0;
-
-    cudaExtent extent = {};
-    extent.width      = data.size(1);
-    extent.height     = data.size(0);
-    extent.depth      = 0;
-
-    CUDA_SAFE_CALL(cudaMalloc3DArray(&output_array, &format, extent));
-
-    CUDA_SAFE_CALL(cudaMemcpy2DToArray(output_array,
-                                       0,
-                                       0,
-                                       data.contiguous().data_ptr(),
-                                       data.size(1) * data.size(2) * data.element_size(),
-                                       data.size(1) * data.size(2) * data.element_size(),
-                                       data.size(0),
-                                       cudaMemcpyDeviceToDevice));
-
-    cudaTextureDesc desc  = {};
-    desc.normalizedCoords = 1;
-    desc.addressMode[0]   = cudaAddressModeClamp;
-    desc.addressMode[1]   = cudaAddressModeClamp;
-    desc.addressMode[2]   = cudaAddressModeClamp;
-    desc.readMode         = isFloat ? cudaReadModeElementType : cudaReadModeNormalizedFloat;
-    desc.filterMode       = cudaFilterModeLinear;
-
-    cudaResourceDesc resDesc = {};
-    resDesc.resType          = cudaResourceTypeArray;
-    resDesc.res.array.array  = output_array;
-
-    CUDA_SAFE_CALL(cudaCreateTextureObject(&output_texture, &resDesc, &desc, nullptr));
-}
-
 __global__ void
 computeMeshTrianglePDFKernel(const torch::PackedTensorAccessor32<float, 2, at::RestrictPtrTraits> positions,
                              const torch::PackedTensorAccessor32<uint32_t, 2, at::RestrictPtrTraits> indices,
@@ -140,7 +78,7 @@ MeshEmitter::MeshEmitter(const Dictionary& dict)
 
     MeshEmitterData data;
 
-    detail::convertToTextureObject(texture_emissive->getData(atcg::GPU), _emissive_texture, data.emissive_texture);
+    atcg::convertToTextureObject(texture_emissive->getData(atcg::GPU), _emissive_texture, data.emissive_texture);
 
     data.emitter_scaling = emission_scaling;
 
