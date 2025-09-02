e7dcef965db120e96473bd8b02d405804b22a5b3
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-09-02 14:40:43 +0200

    Move NdotL to bsdf evaluation

--- atcg_lib/platform/optix/src/Integrator/PathtracingIntegrator_ptx.cu
+++ atcg_lib/platform/optix/src/Integrator/PathtracingIntegrator_ptx.cu
@@ -114,9 +114,8 @@ extern "C" __global__ void __raygen__rg()
                                            : bsdf_result.sample_probability;
                     float mis_weight = emitter_sampling.sampling_pdf / (emitter_sampling.sampling_pdf + bsdf_pdf);
 
-                    radiance += mis_weight * throughput * emitter_sampling.radiance_weight_at_receiver *
-                                bsdf_result.bsdf_value *
-                                glm::max(0.0f, glm::dot(si.normal, emitter_sampling.direction_to_light));
+                    radiance +=
+                        mis_weight * throughput * emitter_sampling.radiance_weight_at_receiver * bsdf_result.bsdf_value;
                 } while(false);
 
                 auto result = si.bsdf->sampleBSDF(si, rng);
