8a733896ca0e9a1a2d70e1866388f4070bfb3e6e
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-09-10 13:05:25 +0200

    Separate Definition of interactions

--- atcg_lib/platform/optix/include/Core/SurfaceInteraction.h
+++ atcg_lib/platform/optix/include/Core/SurfaceInteraction.h
@@ -7,16 +7,21 @@ namespace atcg
 
 struct BSDFVPtrTable;
 struct EmitterVPtrTable;
-struct SurfaceInteraction
+
+struct Interaction
 {
     bool valid = false;
     glm::vec3 position;
+    glm::vec3 incoming_direction;
+    float incoming_distance;
+};
+
+struct SurfaceInteraction : public Interaction
+{
     glm::vec3 normal;
     glm::vec3 color;
     glm::vec2 barys;
     glm::vec2 uv;
-    glm::vec3 incoming_direction;
-    float incoming_distance;
     uint32_t primitive_idx;
     uint32_t entity_id;
 
