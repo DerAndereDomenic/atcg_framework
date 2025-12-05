119661ea5dbb1349c96a4bdd86e24799d6cdfc16
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-12-05 11:36:02 +0100

    Implement ior texture into path tracer

--- atcg_lib/platform/optix/src/BSDF/DielectricBSDF_ptx.cu
+++ atcg_lib/platform/optix/src/BSDF/DielectricBSDF_ptx.cu
@@ -209,7 +209,9 @@ __direct_callable__sample_dielectricbsdf(const atcg::SurfaceInteraction& si, atc
     float roughness             = sbt_data->roughness_texture.read(si.uv);
     roughness = glm::max(roughness * roughness, 1e-3f);    // In the real time shaders, roughness is squared
 
-    return detail::sampleRefractive(si, reflectance_color, roughness, sbt_data->ior, rng);
+    float ior = sbt_data->ior_texture.read(si.uv);
+
+    return detail::sampleRefractive(si, reflectance_color, roughness, ior, rng);
 }
 
 extern "C" __device__ atcg::BSDFEvalResult __direct_callable__eval_dielectricbsdf(const atcg::SurfaceInteraction& si,
@@ -222,5 +224,7 @@ extern "C" __device__ atcg::BSDFEvalResult __direct_callable__eval_dielectricbsd
     float roughness             = sbt_data->roughness_texture.read(si.uv);
     roughness = glm::max(roughness * roughness, 1e-3f);    // In the real time shaders, roughness is squared
 
-    return detail::evalRefractive(si, outgoing_dir, reflectance_color, roughness, sbt_data->ior);
+    float ior = sbt_data->ior_texture.read(si.uv);
+
+    return detail::evalRefractive(si, outgoing_dir, reflectance_color, roughness, ior);
 }
