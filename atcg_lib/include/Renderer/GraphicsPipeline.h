d059d91d752c5d60942a51544559bb22652c84c5
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-12-25 21:31:20 +0100

    Add basic GraphcisPipeline

---
+++ atcg_lib/include/Renderer/GraphicsPipeline.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <Renderer/Shader.h>
+#include <Renderer/RenderState.h>
+
+namespace atcg
+{
+struct GraphicsPipeline
+{
+    atcg::ref_ptr<Shader> shader;
+    RenderState render_state;
+    PrimitiveTopology primitive_type = PrimitiveTopology::ATCG_TRIANGLES;
+
+    ATCG_INLINE GraphicsPipeline() = default;
+
+    ATCG_INLINE GraphicsPipeline setShader(const atcg::ref_ptr<Shader>& shd)
+    {
+        shader = shd;
+        return *this;
+    }
+
+    ATCG_INLINE GraphicsPipeline setRenderState(const RenderState& state)
+    {
+        render_state = state;
+        return *this;
+    }
+
+    ATCG_INLINE GraphicsPipeline setPrimitiveTopology(const PrimitiveTopology& topology)
+    {
+        primitive_type = topology;
+        return *this;
+    }
+
+    ATCG_INLINE bool operator==(const GraphicsPipeline& other) const
+    {
+        return shader == other.shader && render_state == other.render_state && primitive_type == other.primitive_type;
+    }
+};
+}    // namespace atcg
