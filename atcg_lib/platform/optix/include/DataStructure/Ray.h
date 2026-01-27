398614cfba802afb0eaf0a088e24ba80ac232d64
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-01-27 13:25:59 +0100

    Add ray struct

---
+++ atcg_lib/platform/optix/include/DataStructure/Ray.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Core/glm.h>
+
+#include <Medium/MediumVPtrTable.cuh>
+
+namespace atcg
+{
+class Ray
+{
+public:
+    Ray() = default;
+
+    ATCG_HOST_DEVICE Ray(const glm::vec3& origin, const glm::vec3& direction, float tmin = 0.001f, float tmax = 1e16f)
+        : origin(origin),
+          direction(direction),
+          tmin(tmin),
+          tmax(tmax)
+    {
+    }
+
+    ATCG_HOST_DEVICE ATCG_INLINE glm::vec3 operator()(float t) const { return origin + t * direction; }
+
+    glm::vec3 origin;
+    glm::vec3 direction;
+    float tmin                            = 0.001f;
+    float tmax                            = 1e16f;
+    const MediumVPtrTable* current_medium = nullptr;
+};
+}    // namespace atcg
