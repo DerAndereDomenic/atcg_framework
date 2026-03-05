0ed75e3172255398cf1888094391160856a1338a
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-03-05 10:18:16 +0100

    Only compile atomicAdd in device code

--- atcg_lib/platform/optix/include/Core/GlobalAtomicAdd.h
+++ atcg_lib/platform/optix/include/Core/GlobalAtomicAdd.h
@@ -8,7 +8,9 @@ namespace atcg
 // Fix for global memory address atomic add (needed for atomicAdd in backward pass)
 ATCG_DEVICE ATCG_INLINE void globalAtomicAdd(float* addr, float val)
 {
+#ifdef __CUDACC__
     unsigned long long ptr = (unsigned long long)__cvta_generic_to_global(addr);
     asm volatile("atom.global.add.f32 %0, [%1], %2;" : "=f"(val) : "l"(ptr), "f"(val) : "memory");
+#endif
 }
 }    // namespace atcg
