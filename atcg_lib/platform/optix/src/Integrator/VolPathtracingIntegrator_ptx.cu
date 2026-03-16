ab715ed6ef88ca73bec298c141660e9f72acafe2
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-03-16 10:35:06 +0100

    Implement mis for volumes

--- atcg_lib/platform/optix/src/Integrator/VolPathtracingIntegrator_ptx.cu
+++ atcg_lib/platform/optix/src/Integrator/VolPathtracingIntegrator_ptx.cu
@@ -34,8 +34,8 @@ extern "C" __global__ void __raygen__rg()
 
     bool next_ray_valid = true;
 
-    atcg::SurfaceInteraction last_si;
-    last_si.pdf = 1.0f;
+    atcg::AnyInteraction last_ai;
+    last_ai->pdf = 1.0f;
 
     for(int n = 0; n < 512; ++n)
     {
@@ -64,8 +64,7 @@ extern "C" __global__ void __raygen__rg()
         if(si.isValid() &&
            camera_ray.ray.current_medium)    // For now, we only allow media inside objects. So if si not valid, reject
         {
-            float max_distance = si.isValid() ? glm::length(si.position - camera_ray.ray.origin)
-                                              : std::numeric_limits<float>::infinity();
+            float max_distance = glm::length(si.position - camera_ray.ray.origin);
             atcg::MediumSamplingResult result =
                 camera_ray.ray.current_medium->sampleMediumEvent(camera_ray.ray.origin,
                                                                  camera_ray.ray.direction,
@@ -81,13 +80,84 @@ extern "C" __global__ void __raygen__rg()
             {
                 atcg::MediumInteraction mi = result.interaction;
 
-                // TODO: NEE MIS
+                // NEE
+                do
+                {
+                    if(!si.bsdf || (int)(si.bsdf->flags & atcg::BSDFComponentType::NullTransmission) == 0)
+                    {
+                        break;
+                    }
+                    if(params.num_emitters == 0) break;
+
+                    uint32_t emitter_index = rng.nextUint32() % params.num_emitters;
+
+                    float emitter_selection_pdf = 1.0f / ((float)params.num_emitters);
+
+                    const atcg::EmitterVPtrTable* emitter = params.emitters[emitter_index];
+
+                    atcg::EmitterSamplingResult emitter_sampling = emitter->sampleLight(mi, wavelengths, rng);
+
+                    if(emitter_sampling.sampling_pdf == 0)
+                    {
+                        break;
+                    }
+                    emitter_sampling.sampling_pdf *= emitter_selection_pdf;
+
+                    atcg::SurfaceInteraction si_dummy;
+                    atcg::traceWithDataPointer<atcg::SurfaceInteraction>(params.handle,
+                                                                         mi.position,
+                                                                         emitter_sampling.direction_to_light,
+                                                                         0.0f,
+                                                                         1e16f,
+                                                                         &si_dummy,
+                                                                         params.surface_trace_params);
+
+                    if(!si_dummy.isValid())
+                    {
+                        // Should not happen because we are inside the geometry
+                        break;
+                    }
+
+                    bool occluded =
+                        traceOcclusion(params.handle,
+                                       si_dummy.position,
+                                       emitter_sampling.direction_to_light,
+                                       1e-3f,
+                                       emitter_sampling.distance_to_light - si_dummy.incoming_distance - 1e-3f,
+                                       params.occlusion_trace_params);
+
+                    if(occluded)
+                    {
+                        break;
+                    }
+
+                    float transmittance_to_light = camera_ray.ray.current_medium
+                                                       ->evalTransmittance(mi.position,
+                                                                           emitter_sampling.direction_to_light,
+                                                                           si_dummy.incoming_distance,
+                                                                           rng)
+                                                       .x;    // TODO
+
+                    auto phase_result = camera_ray.ray.current_medium->phase_function->evalPhaseFunction(
+                        mi,
+                        emitter_sampling.direction_to_light);
+                    float phase_pdf    = phase_result.sampling_pdf;
+                    float sampling_pdf = (int)(emitter->flags & atcg::EmitterFlags::InfinitesimalSize) != 0
+                                             ? 0.0f
+                                             : phase_pdf;    // * transmittance_to_light;
+
+                    float mis_weight = emitter_sampling.sampling_pdf / (emitter_sampling.sampling_pdf + sampling_pdf);
+
+                    radiance += mis_weight * camera_ray.importance * transmittance_to_light *
+                                phase_result.phase_function_value * emitter_sampling.radiance_weight_at_receiver;
+                } while(false);
 
                 const atcg::PhaseFunctionVPtrTable* phase_function = camera_ray.ray.current_medium->phase_function;
                 atcg::PhaseFunctionSamplingResult phase_result     = phase_function->samplePhaseFunction(mi, rng);
                 if(phase_result.sampling_pdf == 0)
                 {
                     next_ray_valid = false;
+                    last_ai->setInvalid();
                     break;
                 }
 
@@ -96,7 +166,8 @@ extern "C" __global__ void __raygen__rg()
                 camera_ray.importance *= phase_result.phase_function_weight;
                 next_ray_valid = true;
 
-                // TODO MIS
+                last_ai      = mi;
+                last_ai->pdf = phase_result.sampling_pdf;
 
                 continue;
             }
@@ -106,12 +177,12 @@ extern "C" __global__ void __raygen__rg()
         {
             if(params.environment_emitter)
             {
-                bool mis_valid              = last_si.isValid();
+                bool mis_valid              = last_ai->isValid();
                 float emitter_selection_pdf = 1.0f / ((float)params.num_emitters);
                 float emitter_sampling_pdf =
-                    mis_valid ? params.environment_emitter->evalLightSamplingPdf(last_si, si) * emitter_selection_pdf
+                    mis_valid ? params.environment_emitter->evalLightSamplingPdf(last_ai, si) * emitter_selection_pdf
                               : 0.0f;
-                float mis_weight = last_si.pdf / (last_si.pdf + emitter_sampling_pdf);
+                float mis_weight = last_ai->pdf / (last_ai->pdf + emitter_sampling_pdf);
                 radiance += mis_weight * camera_ray.importance * params.environment_emitter->evalLight(si, wavelengths);
             }
             next_ray_valid = false;
@@ -126,9 +197,10 @@ extern "C" __global__ void __raygen__rg()
         // Check for light source
         if(si.emitter)
         {
-            bool mis_valid             = last_si.isValid();
-            float emitter_sampling_pdf = mis_valid ? si.emitter->evalLightSamplingPdf(last_si, si) : 0.0f;
-            float mis_weight           = last_si.pdf / (last_si.pdf + emitter_sampling_pdf);
+            bool mis_valid              = last_ai->isValid();
+            float emitter_selection_pdf = 1.0f / ((float)params.num_emitters);
+            float emitter_sampling_pdf  = mis_valid ? si.emitter->evalLightSamplingPdf(last_ai, si) : 0.0f;
+            float mis_weight            = last_ai->pdf / (last_ai->pdf + emitter_sampling_pdf * emitter_selection_pdf);
             radiance += mis_weight * camera_ray.importance * si.emitter->evalLight(si, wavelengths);
         }
 
@@ -187,8 +259,18 @@ extern "C" __global__ void __raygen__rg()
                 camera_ray.importance *= result.bsdf_weight;
                 next_ray_valid = true;
 
-                last_si     = si;
-                last_si.pdf = result.sample_probability;
+                if((int)(result.flags & atcg::BSDFComponentType::NullTransmission) == 0)
+                {
+                    // If the sampled component is a null transmission, we don't want to count it because for NEE we
+                    // need the last non-null-transportation interaction. This is a bit hacky but it works for now.
+                    last_ai      = si;
+                    last_ai->pdf = result.sample_probability;
+
+                    if((int)(result.flags & atcg::BSDFComponentType::AnyDelta) != 0)
+                    {
+                        last_ai->setInvalid();    // Invalidate last_ai to prevent NEE for delta interactions
+                    }
+                }
 
                 // Check if we are entering the geometry or leaving the geometry and assign si.inside_medium or
                 // si.outside_medium, respectively.
@@ -198,11 +280,6 @@ extern "C" __global__ void __raygen__rg()
                 if(cos_theta_curr_ray * cos_theta_next_ray > 0)
                 {
                     camera_ray.ray.current_medium = cos_theta_next_ray < 0 ? si.inside_medium : si.outside_medium;
-                }
-
-                if((int)(result.flags & atcg::BSDFComponentType::AnyDelta) != 0)
-                {
-                    last_si.setInvalid();    // Invalidate last_si to prevent NEE for delta interactions
                 }
             }
         }
