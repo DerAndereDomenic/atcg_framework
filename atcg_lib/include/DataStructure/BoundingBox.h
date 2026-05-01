086a50938e9f78dafb9249dc4b186d6787769be7
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-05-01 15:15:57 +0200

    Add bounding boxes and render them to geometry

--- atcg_lib/include/DataStructure/BoundingBox.h
+++ atcg_lib/include/DataStructure/BoundingBox.h
@@ -12,4 +12,32 @@ struct BoundingBox
     glm::vec3 min = glm::vec3(-1);
     glm::vec3 max = glm::vec3(1);
 };
+
+ATCG_INLINE BoundingBox operator+(const BoundingBox& a, const BoundingBox& b)
+{
+    BoundingBox result;
+    result.min = glm::min(a.min, b.min);
+    result.max = glm::max(a.max, b.max);
+    return result;
+}
+
+ATCG_INLINE BoundingBox transformBoundingBox(const BoundingBox& bbox, const glm::mat4& transform)
+{
+    glm::vec3 min = transform * glm::vec4(bbox.min, 1.0f);
+    glm::vec3 max = transform * glm::vec4(bbox.max, 1.0f);
+
+    BoundingBox result;
+    result.min = glm::min(min, max);
+    result.max = glm::max(min, max);
+    return result;
+}
+
+ATCG_INLINE glm::mat4 boundingBoxToModelMatrix(const BoundingBox& bbox)
+{
+    glm::vec3 center = (bbox.min + bbox.max) * 0.5f;
+    glm::vec3 scale  = bbox.max - bbox.min;
+
+    glm::mat4 model = glm::translate(center) * glm::scale(scale);
+    return model;
+}
 }    // namespace atcg
