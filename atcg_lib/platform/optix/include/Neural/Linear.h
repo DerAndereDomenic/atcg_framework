c68ef81c4e98bdc9a9b541f76d0504de0b3656f8
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-06-12 13:36:33 +0200

    Add coop vec mul without bias

--- atcg_lib/platform/optix/include/Neural/Linear.h
+++ atcg_lib/platform/optix/include/Neural/Linear.h
@@ -40,5 +40,27 @@ ATCG_DEVICE OptixCoopVec<half, output_size> coopVecMatMul(const OptixCoopVec<hal
     return output;
 }
 
+template<int output_size,
+         int input_size,
+         bool transpose                  = false,
+         OptixCoopVecMatrixLayout layout = OPTIX_COOP_VEC_MATRIX_LAYOUT_INFERENCING_OPTIMAL>
+ATCG_DEVICE OptixCoopVec<half, output_size>
+coopVecMatMul(const OptixCoopVec<half, input_size>& input, CUdeviceptr weights, size_t weights_offset)
+{
+    using T_INPUT  = OptixCoopVec<half, input_size>;
+    using T_OUTPUT = OptixCoopVec<half, output_size>;
+
+    T_OUTPUT output =
+        optixCoopVecMatMul<T_OUTPUT,
+                           T_INPUT,
+                           OPTIX_COOP_VEC_ELEM_TYPE_FLOAT16,
+                           layout,
+                           transpose,
+                           output_size,
+                           input_size,
+                           OPTIX_COOP_VEC_ELEM_TYPE_FLOAT16>(input, weights, weights_offset, sizeof(half) * input_size);
+    return output;
+}
+
 #endif
 }    // namespace atcg
