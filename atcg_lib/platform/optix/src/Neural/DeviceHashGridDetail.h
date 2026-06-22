592492a21de18b4876aff1a8c5d1121d6478c7a7
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-06-22 11:09:20 +0200

    Use float to interpolate grid weights in hash grid

--- atcg_lib/platform/optix/src/Neural/DeviceHashGridDetail.h
+++ atcg_lib/platform/optix/src/Neural/DeviceHashGridDetail.h
@@ -8,13 +8,7 @@ namespace atcg
 
 ATCG_DEVICE uint32_t _hash(const glm::ivec3& pos, uint32_t T_size)
 {
-    return ((pos.x) ^ (pos.y * 2654435761) ^ (pos.z * 805459861)) % T_size;
-}
-
-template<typename T>
-ATCG_DEVICE T _mix(const T& a, const T& b, const T& t)
-{
-    return a * (T(1.0f) - t) + b * t;
+    return (((uint32_t)pos.x) ^ ((uint32_t)pos.y * 2654435761) ^ ((uint32_t)pos.z * 805459861)) % T_size;
 }
 
 template<typename T, uint32_t L, uint32_t F>
@@ -47,24 +41,24 @@ ATCG_DEVICE OptixCoopVec<T, L * F> DeviceHashGrid<T, L, F>::forward(const glm::v
         // Interpolate
         for(int f = 0; f < F; ++f)
         {
-            T c000 = layer_weights[hash000 * F + f];
-            T c001 = layer_weights[hash001 * F + f];
-            T c010 = layer_weights[hash010 * F + f];
-            T c011 = layer_weights[hash011 * F + f];
-            T c100 = layer_weights[hash100 * F + f];
-            T c101 = layer_weights[hash101 * F + f];
-            T c110 = layer_weights[hash110 * F + f];
-            T c111 = layer_weights[hash111 * F + f];
+            float c000 = (float)layer_weights[hash000 * F + f];
+            float c001 = (float)layer_weights[hash001 * F + f];
+            float c010 = (float)layer_weights[hash010 * F + f];
+            float c011 = (float)layer_weights[hash011 * F + f];
+            float c100 = (float)layer_weights[hash100 * F + f];
+            float c101 = (float)layer_weights[hash101 * F + f];
+            float c110 = (float)layer_weights[hash110 * F + f];
+            float c111 = (float)layer_weights[hash111 * F + f];
 
-            T c00 = _mix(c000, c100, (T)int_weights.x);
-            T c01 = _mix(c001, c101, (T)int_weights.x);
-            T c10 = _mix(c010, c110, (T)int_weights.x);
-            T c11 = _mix(c011, c111, (T)int_weights.x);
+            float c00 = glm::mix(c000, c100, int_weights.x);
+            float c01 = glm::mix(c001, c101, int_weights.x);
+            float c10 = glm::mix(c010, c110, int_weights.x);
+            float c11 = glm::mix(c011, c111, int_weights.x);
 
-            T c0 = _mix(c00, c10, (T)int_weights.y);
-            T c1 = _mix(c01, c11, (T)int_weights.y);
+            float c0 = glm::mix(c00, c10, int_weights.y);
+            float c1 = glm::mix(c01, c11, int_weights.y);
 
-            result[l * F + f] = _mix(c0, c1, (T)int_weights.z);
+            result[l * F + f] = (T)glm::mix(c0, c1, int_weights.z);
         }
     }
 
@@ -103,66 +97,66 @@ ATCG_DEVICE glm::vec3 DeviceHashGrid<T, L, F>::backward(const glm::vec3& positio
 
         for(int f = 0; f < F; ++f)
         {
-            T g = grad_output[l * F + f];
+            float g = (float)grad_output[l * F + f];
 
             // load corners
-            T c000 = layer_weights[hash000 * F + f];
-            T c001 = layer_weights[hash001 * F + f];
-            T c010 = layer_weights[hash010 * F + f];
-            T c011 = layer_weights[hash011 * F + f];
-            T c100 = layer_weights[hash100 * F + f];
-            T c101 = layer_weights[hash101 * F + f];
-            T c110 = layer_weights[hash110 * F + f];
-            T c111 = layer_weights[hash111 * F + f];
+            float c000 = (float)layer_weights[hash000 * F + f];
+            float c001 = (float)layer_weights[hash001 * F + f];
+            float c010 = (float)layer_weights[hash010 * F + f];
+            float c011 = (float)layer_weights[hash011 * F + f];
+            float c100 = (float)layer_weights[hash100 * F + f];
+            float c101 = (float)layer_weights[hash101 * F + f];
+            float c110 = (float)layer_weights[hash110 * F + f];
+            float c111 = (float)layer_weights[hash111 * F + f];
 
             // ---- forward reconstruction structure ----
-            T c00 = _mix(c000, c100, (T)w.x);
-            T c01 = _mix(c001, c101, (T)w.x);
-            T c10 = _mix(c010, c110, (T)w.x);
-            T c11 = _mix(c011, c111, (T)w.x);
+            float c00 = glm::mix(c000, c100, w.x);
+            float c01 = glm::mix(c001, c101, w.x);
+            float c10 = glm::mix(c010, c110, w.x);
+            float c11 = glm::mix(c011, c111, w.x);
 
-            T c0 = _mix(c00, c10, (T)w.y);
-            T c1 = _mix(c01, c11, (T)w.y);
+            float c0 = glm::mix(c00, c10, w.y);
+            float c1 = glm::mix(c01, c11, w.y);
 
-            T c = _mix(c0, c1, (T)w.z);
+            float c = glm::mix(c0, c1, w.z);
 
 
             // =========================
             // Backprop through mix
             // =========================
 
-            T dc0 = g * (T(1.0f) - (T)w.z);
-            T dc1 = g * (T)w.z;
+            float dc0 = g * (1.0f - w.z);
+            float dc1 = g * w.z;
 
-            T dc00 = dc0 * (T(1.0f) - (T)w.y);
-            T dc10 = dc0 * (T)w.y;
-            T dc01 = dc1 * (T(1.0f) - (T)w.y);
-            T dc11 = dc1 * (T)w.y;
+            float dc00 = dc0 * (1.0f - w.y);
+            float dc10 = dc0 * w.y;
+            float dc01 = dc1 * (1.0f - w.y);
+            float dc11 = dc1 * w.y;
 
-            T dc000 = dc00 * (T(1.0f) - (T)w.x);
-            T dc100 = dc00 * (T)w.x;
-            T dc001 = dc01 * (T(1.0f) - (T)w.x);
-            T dc101 = dc01 * (T)w.x;
-            T dc010 = dc10 * (T(1.0f) - (T)w.x);
-            T dc110 = dc10 * (T)w.x;
-            T dc011 = dc11 * (T(1.0f) - (T)w.x);
-            T dc111 = dc11 * (T)w.x;
+            float dc000 = dc00 * (1.0f - w.x);
+            float dc100 = dc00 * w.x;
+            float dc001 = dc01 * (1.0f - w.x);
+            float dc101 = dc01 * w.x;
+            float dc010 = dc10 * (1.0f - w.x);
+            float dc110 = dc10 * w.x;
+            float dc011 = dc11 * (1.0f - w.x);
+            float dc111 = dc11 * w.x;
 
             if constexpr(accumulate)
             {
-                atomicAdd(&layer_grads[hash000 * F + f], (float)dc000);
-                atomicAdd(&layer_grads[hash001 * F + f], (float)dc001);
-                atomicAdd(&layer_grads[hash010 * F + f], (float)dc010);
-                atomicAdd(&layer_grads[hash011 * F + f], (float)dc011);
-                atomicAdd(&layer_grads[hash100 * F + f], (float)dc100);
-                atomicAdd(&layer_grads[hash101 * F + f], (float)dc101);
-                atomicAdd(&layer_grads[hash110 * F + f], (float)dc110);
-                atomicAdd(&layer_grads[hash111 * F + f], (float)dc111);
+                atomicAdd(&layer_grads[hash000 * F + f], dc000);
+                atomicAdd(&layer_grads[hash001 * F + f], dc001);
+                atomicAdd(&layer_grads[hash010 * F + f], dc010);
+                atomicAdd(&layer_grads[hash011 * F + f], dc011);
+                atomicAdd(&layer_grads[hash100 * F + f], dc100);
+                atomicAdd(&layer_grads[hash101 * F + f], dc101);
+                atomicAdd(&layer_grads[hash110 * F + f], dc110);
+                atomicAdd(&layer_grads[hash111 * F + f], dc111);
             }
 
             glm::vec3 dpos_scaled(0.0f);
 
-            T dwdx = 0, dwdy = 0, dwdz = 0;
+            float dwdx = 0, dwdy = 0, dwdz = 0;
 
             // x
             dwdx += (dc100 + dc101 + dc110 + dc111) - (dc000 + dc001 + dc010 + dc011);
