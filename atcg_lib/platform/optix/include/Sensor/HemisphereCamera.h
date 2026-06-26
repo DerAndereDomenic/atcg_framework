8a81ac2ade5102ec922f4d02fbcd430931a2e7b7
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-06-26 15:22:05 +0200

    Add hemisphere camera

---
+++ atcg_lib/platform/optix/include/Sensor/HemisphereCamera.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Sensor/Sensor.h>
+#include <Sensor/HemisphereCameraData.cuh>
+
+namespace atcg
+{
+class ATCG_API HemisphereCamera : public Sensor
+{
+public:
+    /**
+     * @brief Constructor from a dictionary.
+     * The dictionary expects
+     * - position: glm::vec3
+     * - normal: glm::vec3
+     * - film: atcg::ref_ptr<Film>
+     *
+     * @param dict The sensor data
+     */
+    HemisphereCamera(const Dictionary& dict);
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~HemisphereCamera();
+
+    /**
+     * @brief A callback to display debug information in imgui
+     */
+    virtual void onImGuiRender() override;
+
+    /**
+     * @brief Initialize the pipeline
+     *
+     * @param pipeline The raytracing pipeline
+     * @param sbt The shader binding table
+     */
+    virtual void initializePipeline(const atcg::ref_ptr<RayTracingPipeline>& pipeline,
+                                    const atcg::ref_ptr<ShaderBindingTable>& sbt) override;
+
+    /**
+     * @brief Mark the sensor as dirty (e.g. camera changed)
+     */
+    virtual void markDirty() override;
+
+
+private:
+    glm::vec3 _position;
+    glm::vec3 _normal;
+    float _exposure = 1.0f;
+    atcg::dref_ptr<HemisphereCameraData> _hemisphere_camera_data;
+};
+
+}    // namespace atcg
---
+++ atcg_lib/platform/optix/src/Sensor/HemisphereCamera.cpp
@@ -0,0 +1,52 @@
+#include <Sensor/HemisphereCamera.h>
+#include <Renderer/PerspectiveCamera.h>
+
+namespace atcg
+{
+HemisphereCamera::HemisphereCamera(const Dictionary& dict)
+{
+    _position = dict.getValue<glm::vec3>("position");
+    _normal   = dict.getValue<glm::vec3>("normal");
+    _exposure = dict.getValueOr<float>("exposure", 1.0f);
+    _film     = dict.getValue<atcg::ref_ptr<Film>>("film");
+}
+
+HemisphereCamera::~HemisphereCamera() {}
+
+void HemisphereCamera::onImGuiRender() {}
+
+void HemisphereCamera::markDirty()
+{
+    HemisphereCameraData hemisphere_camera_data;
+
+    hemisphere_camera_data.cam_eye  = _position;
+    hemisphere_camera_data.normal   = _normal;
+    hemisphere_camera_data.exposure = _exposure;
+    hemisphere_camera_data.film     = _film->getVPtrTable();
+
+    _hemisphere_camera_data.upload(&hemisphere_camera_data);
+}
+
+void HemisphereCamera::initializePipeline(const atcg::ref_ptr<RayTracingPipeline>& pipeline,
+                                          const atcg::ref_ptr<ShaderBindingTable>& sbt)
+{
+    if(!_film) return;
+    _film->ensureInitialized(pipeline, sbt);
+    markDirty();
+
+    const std::string ptx_sensor_filename = "./bin/HemisphereCamera_ptx.ptx";
+    auto generate_ray_prog_group =
+        pipeline->addCallableShader({ptx_sensor_filename, "__direct_callable__generate_ray_hemisphere"});
+    auto add_sample_prog_group =
+        pipeline->addCallableShader({ptx_sensor_filename, "__direct_callable__add_sample_hemisphere"});
+    uint32_t generate_ray_idx = sbt->addCallableEntry(generate_ray_prog_group, _hemisphere_camera_data.get());
+    uint32_t add_sample_idx   = sbt->addCallableEntry(add_sample_prog_group, _hemisphere_camera_data.get());
+
+    SensorVPtrTable vptr_table;
+    vptr_table.generateRayCallIndex = generate_ray_idx;
+    vptr_table.addSampleCallIndex   = add_sample_idx;
+
+    _vptr_table.upload(&vptr_table);
+    markInitialized();
+}
+}    // namespace atcg
