ccbf460411a6c33b1e0f0b11585781e50456d220
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-07-08 10:58:14 +0200

    Add first basic version of PPM

---
+++ atcg_lib/platform/optix/include/Integrator/PhotonMapData.cuh
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <optix.h>
+#include <Core/glm.h>
+#include <Core/TraceParameters.h>
+#include <Emitter/EmitterVPtrTable.cuh>
+#include <BSDF/BSDFVPtrTable.cuh>
+#include <Sensor/SensorVPtrTable.cuh>
+#include <cuBQL/bvh.h>
+#include <cuBQL/math/box.h>
+
+#define PHOTON_MAP_MAX_NUM_PHOTONS    1000000
+#define PHOTON_MAP_TRACE_DEPTH        8
+#define PHOTON_MAP_PHOTONS_PER_LAUNCH ((PHOTON_MAP_MAX_NUM_PHOTONS) / (PHOTON_MAP_TRACE_DEPTH))
+#define PHOTON_MAP_GATHER_RADIUS      0.25f
+#define PHOTON_MAP_GATHER_RADIUS_SQ   ((PHOTON_MAP_GATHER_RADIUS) * (PHOTON_MAP_GATHER_RADIUS))
+#define PHOTON_MAP_REDUCTION_FACTOR   0.7f
+
+namespace atcg
+{
+
+struct PhotonMapData
+{
+    glm::vec3 position;
+    glm::vec3 direction;
+    glm::vec3 normal;
+    SampledSpectrum throughput;
+};
+
+struct PhotonGatherData
+{
+    uint32_t photon_count          = 0;
+    float gather_radius_sq         = PHOTON_MAP_GATHER_RADIUS_SQ;
+    SampledSpectrum gathered_power = SampledSpectrum(0.0f);
+};
+
+struct PhotonMapParams
+{
+    uint32_t image_width;
+    uint32_t image_height;
+
+    int32_t* entity_ids;
+
+    OptixTraversableHandle handle;
+
+    TraceParameters surface_trace_params;
+    TraceParameters occlusion_trace_params;
+
+    uint32_t frame_counter;
+
+    // Emitter
+    uint32_t num_emitters;
+    const EmitterVPtrTable** emitters;
+
+    const EmitterVPtrTable* environment_emitter;
+
+    const SensorVPtrTable* sensor;
+
+    // Photon data
+    uint32_t photons_per_launch;
+    uint32_t max_num_photons;
+
+    PhotonMapData* photon_data;
+    cuBQL::box3f* photon_bounds;
+    cuBQL::bvh3f* photon_bvh;
+    int* photon_index;
+
+    PhotonGatherData* photon_gather_data;
+};
+}    // namespace atcg
---
+++ atcg_lib/platform/optix/include/Integrator/PhotonMapIntegrator.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <Integrator/Integrator.h>
+#include <Shape/IAS.h>
+#include <Integrator/PhotonMapData.cuh>
+#include <Emitter/EnvironmentEmitter.h>
+#include <Emitter/PointEmitter.h>
+#include <Scene/OptixScene.h>
+#include <cuBQL/bvh.h>
+
+namespace atcg
+{
+/**
+ * @brief A simple photon mapping integrator
+ */
+class ATCG_API PhotonMapIntegrator : public Integrator
+{
+public:
+    /**
+     * @brief Constructor
+     *
+     * @param context The raytracing context
+     * @param dict Additional parameters
+     */
+    PhotonMapIntegrator(const atcg::ref_ptr<RaytracingContext>& context, const atcg::Dictionary& dict);
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~PhotonMapIntegrator();
+
+
+    /**
+     * @brief A callback to display debug information in imgui
+     */
+    virtual void onImGuiRender() override;
+
+    /**
+     * @brief Generate the rays and write to some output tensors
+     * This integrator expects:
+     * camera - atcg::ref_ptr<PerspectiveCamera>
+     * output - torch::Tensor
+     *
+     * @param in_out_dictionary The input output dictionary
+     */
+    virtual void generateRays(Dictionary& in_out_dictionary) override;
+
+    /**
+     * @brief Reset the internal structure of the integrator
+     */
+    virtual void reset() override;
+
+private:
+    /**
+     * @brief Initialize a pipeline.
+     */
+    void initializePipeline(const Dictionary& dict);
+
+    void generateNewPhotonMap();
+
+    void tracePhotons();
+
+    void buildPhotonMap();
+
+    uint32_t _raygen_index;
+    uint32_t _raygen_sample_photons_index;
+    uint32_t _raygen_photon_request_index;
+    uint32_t _surface_miss_index;
+    uint32_t _occlusion_miss_index;
+
+    atcg::ref_ptr<Scene> _scene;
+    atcg::ref_ptr<OptixScene> _optix_scene;
+    atcg::dref_ptr<PhotonMapParams> _launch_params;
+    uint32_t _frame_counter = 0;
+
+    atcg::DeviceBuffer<PhotonMapData> _photon_data;
+    atcg::DeviceBuffer<cuBQL::box3f> _photon_bounds;
+    atcg::DeviceBuffer<PhotonGatherData> _photon_gather_data;
+    atcg::dref_ptr<int> _photon_index;
+    atcg::dref_ptr<cuBQL::bvh3f> _device_photon_bvh;
+    cuBQL::bvh3f _photon_bvh;
+};
+}    // namespace atcg
