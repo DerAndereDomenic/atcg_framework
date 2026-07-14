03840c64d5af0a56ac584c8de34960589c96f527
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-07-14 10:28:51 +0200

    Add DifferentiableIntegrator include

--- atcg_lib/include/ATCG.h
+++ atcg_lib/include/ATCG.h
@@ -126,6 +126,7 @@
     #include <Integrator/PathtracingIntegrator.h>
     #include <Integrator/VolPathtracingIntegrator.h>
     #include <Integrator/PhotonMapIntegrator.h>
+    #include <Integrator/DifferentiableIntegrator.h>
     #include <Core/RaytracingContextManager.h>
 #endif
 
