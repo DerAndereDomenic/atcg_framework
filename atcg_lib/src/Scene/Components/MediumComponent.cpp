50d01737866717b90b3ca328c28d90fe05c5ba64
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-08-12 13:31:20 +0200

    Deserialize Homogeneous and Heterogeneous components for backwards compatibility

--- atcg_lib/src/Scene/Components/MediumComponent.cpp
+++ atcg_lib/src/Scene/Components/MediumComponent.cpp
@@ -2,7 +2,27 @@
 #include <Scene/ComponentRegistry.h>
 #include <Utils/Utils.h>
 
+#include <Material/MediumRegistry.h>
+
 #define MEDIUM_KEY "Medium"
+
+#define HOMOGENEOUS_MEDIUM_KEY "Homogeneous Medium"
+#define ALBEDO_KEY             "albedo"
+#define DENSITY_KEY            "density"
+#define G_KEY                  "g"
+#define LE_KEY                 "Le"
+#define LE_COLOR_KEY           "Le_color"
+
+#define HETEROGENEOUS_MEDIUM_KEY "Heterogeneous Medium"
+#define DENSITY_GRID_KEY         "density_grid"
+#define ALBEDO_GRID_KEY          "albedo_grid"
+#define EMISSION_GRID_KEY        "emission_grid"
+#define G_KEY                    "g"
+#define GRID_KEY                 "grid"
+#define SCALE_KEY                "scale"
+#define BBOX_KEY                 "bbox"
+#define BBOX_MIN_KEY             "min"
+#define BBOX_MAX_KEY             "max"
 
 namespace atcg
 {
@@ -31,13 +51,44 @@ void ComponentSerializer<MediumComponent>::deserialize_component(const std::stri
                                                                  Entity entity,
                                                                  nlohmann::json& j) const
 {
-    if(!j.contains(MEDIUM_KEY))
+    if(j.contains(MEDIUM_KEY))
     {
+        auto& medium         = entity.addComponent<MediumComponent>();
+        medium.medium_handle = (AssetHandle)j[MEDIUM_KEY];
         return;
     }
 
-    auto& medium         = entity.addComponent<MediumComponent>();
-    medium.medium_handle = (AssetHandle)j[MEDIUM_KEY];
+    if(j.contains(HOMOGENEOUS_MEDIUM_KEY) && j.contains(HETEROGENEOUS_MEDIUM_KEY))
+    {
+        ATCG_WARN("Entity contains both Homogeneous and Heterogeneous Medium components. Only one should be present. "
+                  "-- Skipping deserialization of MediumComponent.");
+        return;
+    }
+
+    // These two are for backwards compatibility with the HomogeneousMediumComponent
+    if(j.contains(HOMOGENEOUS_MEDIUM_KEY))
+    {
+        auto medium_json = j[HOMOGENEOUS_MEDIUM_KEY];
+
+        auto& component = entity.addComponent<MediumComponent>();
+
+        auto medium = atcg::MediumRegistry::deserializeMedium("Homogeneous", file_path, medium_json);
+        atcg::AssetManager::registerAsset(medium, "medium");
+        component.medium_handle = medium->handle;
+        return;
+    }
+
+    if(j.contains(HETEROGENEOUS_MEDIUM_KEY))
+    {
+        auto medium_json = j[HETEROGENEOUS_MEDIUM_KEY];
+
+        auto& component = entity.addComponent<MediumComponent>();
+
+        auto medium = atcg::MediumRegistry::deserializeMedium("Heterogeneous", file_path, medium_json);
+        atcg::AssetManager::registerAsset(medium, "medium");
+        component.medium_handle = medium->handle;
+        return;
+    }
 }
 
 
