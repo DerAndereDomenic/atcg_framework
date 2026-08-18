692a238e0f2e317c47d8a0edc146bf7d7a6ac640
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-08-18 09:39:27 +0200

    Add transformation matrix to medium and add medium instance

--- atcg_lib/include/Material/MediumVPtrTable.h
+++ atcg_lib/include/Material/MediumVPtrTable.h
@@ -23,14 +23,19 @@ struct MediumVPtrTable
 #ifdef __CUDACC__
 
     // Evaluate the transmittance over a certain distance inside of this medium starting at the (medium) interaction.
-    __device__ float
-    evalTransmittance(const glm::vec3& origin, const glm::vec3& direction, float distance, PCG32& rng) const
+    __device__ float evalTransmittance(const glm::vec3& origin,
+                                       const glm::vec3& direction,
+                                       float distance,
+                                       const glm::mat4& world_to_object,
+                                       PCG32& rng) const
     {
-        return optixDirectCall<float, const glm::vec3&, const glm::vec3&, float, PCG32&>(evalCallIndex,
-                                                                                         origin,
-                                                                                         direction,
-                                                                                         distance,
-                                                                                         rng);
+        return optixDirectCall<float, const glm::vec3&, const glm::vec3&, float, const glm::mat4&, PCG32&>(
+            evalCallIndex,
+            origin,
+            direction,
+            distance,
+            world_to_object,
+            rng);
     }
 
     // Sample the position of a new medium event starting at the given (medium) interaction.
@@ -38,6 +43,7 @@ struct MediumVPtrTable
                                                       const glm::vec3& direction,
                                                       float max_distance,
                                                       const atcg::SampledWavelengths& wavelengths,
+                                                      const glm::mat4& world_to_object,
                                                       PCG32& rng) const
     {
         return optixDirectCall<MediumSamplingResult,
@@ -45,9 +51,43 @@ struct MediumVPtrTable
                                const glm::vec3&,
                                float,
                                const atcg::SampledWavelengths&,
-                               PCG32&>(sampleCallIndex, origin, direction, max_distance, wavelengths, rng);
+                               const glm::mat4&,
+                               PCG32&>(sampleCallIndex,
+                                       origin,
+                                       direction,
+                                       max_distance,
+                                       wavelengths,
+                                       world_to_object,
+                                       rng);
     }
 
 #endif    // __CUDACC__
 };
+
+
+struct MediumInstance
+{
+    const MediumVPtrTable* vptr_table = nullptr;
+    glm::mat4 world_to_object         = glm::mat4(1);
+
+#ifdef __CUDACC__
+    __device__ float
+    evalTransmittance(const glm::vec3& origin, const glm::vec3& direction, float distance, PCG32& rng) const
+    {
+        return vptr_table->evalTransmittance(origin, direction, distance, world_to_object, rng);
+    }
+
+    __device__ MediumSamplingResult sampleMediumEvent(const glm::vec3& origin,
+                                                      const glm::vec3& direction,
+                                                      float max_distance,
+                                                      const atcg::SampledWavelengths& wavelengths,
+                                                      PCG32& rng) const
+    {
+        return vptr_table->sampleMediumEvent(origin, direction, max_distance, wavelengths, world_to_object, rng);
+    }
+#endif
+
+    __device__ operator bool() const { return vptr_table != nullptr; }
+};
+
 }    // namespace atcg
