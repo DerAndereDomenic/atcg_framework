b8a343fa48efe655068c41d313f95e5e4227c4bc
Domenic Zingsheim <domeniczingsheim@gmail.com>
2026-09-22 13:36:02 +0200

    Move mesh emitter kernels into separate cu file

---
+++ atcg_lib/platform/optix/src/Emitter/MeshEmitter.cpp
@@ -0,0 +1,84 @@
+#include <Emitter/MeshEmitter.h>
+#include <Emitter/MeshEmitterKernels.h>
+#include <ATen/cuda/ApplyGridUtils.cuh>
+#include <c10/cuda/CUDAGuard.h>
+#include <Shape/MeshShape.h>
+#include <Core/Common.h>
+
+namespace atcg
+{
+MeshEmitter::MeshEmitter(const Dictionary& dict)
+{
+    auto shape            = dict.getValue<atcg::ref_ptr<MeshShape>>("shape");
+    auto texture_emissive = dict.getValue<atcg::ref_ptr<Texture2D>>("texture_emissive");
+    auto emission_scaling = dict.getValue<float>("emission_scaling");
+    glm::mat4 transform   = dict.getValue<glm::mat4>("transform");
+
+    MeshEmitterData data;
+
+    _emissive_texture                          = std::dynamic_pointer_cast<Texture2D>(texture_emissive->clone());
+    data.emissive_texture.texture_data.texture = _emissive_texture->getTextureObject();
+    data.emissive_texture.spec                 = _emissive_texture->getSpecification();
+
+    data.emitter_scaling = emission_scaling;
+
+    torch::Tensor positions = shape->getPositions();
+    torch::Tensor uvs       = shape->getUVs();
+    torch::Tensor faces     = shape->getFaces();
+
+    data.positions = (glm::vec3*)positions.data_ptr();
+    data.uvs       = (glm::vec3*)uvs.data_ptr();
+    data.faces     = (glm::u32vec3*)faces.data_ptr();
+    data.num_faces = faces.size(0);
+
+    _mesh_cdf = torch::zeros({faces.size(0)}, atcg::TensorOptions::floatDeviceOptions());
+
+    auto device = _mesh_cdf.device();
+
+    computeMeshTrianglePDFKernel(positions, faces, transform, _mesh_cdf);
+    computeMeshTriangleCDFKernel(_mesh_cdf);
+
+    data.total_area = _mesh_cdf.index({_mesh_cdf.size(0) - 1}).cpu().item<float>();
+    normalizeMeshTriangleCDFKernel(_mesh_cdf, data.total_area);
+
+    data.mesh_cdf       = (float*)_mesh_cdf.data_ptr();
+    data.local_to_world = transform;
+    data.world_to_local = glm::inverse(transform);
+
+    _mesh_emitter_data.upload(&data);
+}
+
+MeshEmitter::~MeshEmitter()
+{
+    _emissive_texture->unmapDevicePointers();
+}
+
+void MeshEmitter::initializePipeline(const atcg::ref_ptr<RayTracingPipeline>& pipeline,
+                                     const atcg::ref_ptr<ShaderBindingTable>& sbt)
+{
+    const std::string ptx_emitter_filename = "./bin/MeshEmitter_ptx.ptx";
+    auto sample_prog_group =
+        pipeline->addCallableShader({ptx_emitter_filename, "__direct_callable__sample_meshemitter"});
+    auto eval_prog_group = pipeline->addCallableShader({ptx_emitter_filename, "__direct_callable__eval_meshemitter"});
+    auto evalpdf_prog_group =
+        pipeline->addCallableShader({ptx_emitter_filename, "__direct_callable__evalpdf_meshemitter"});
+    auto samplephoton_prog_group =
+        pipeline->addCallableShader({ptx_emitter_filename, "__direct_callable__samplephoton_meshemitter"});
+
+    uint32_t sample_idx       = sbt->addCallableEntry(sample_prog_group, _mesh_emitter_data.get());
+    uint32_t eval_idx         = sbt->addCallableEntry(eval_prog_group, _mesh_emitter_data.get());
+    uint32_t eval_pdf_idx     = sbt->addCallableEntry(evalpdf_prog_group, _mesh_emitter_data.get());
+    uint32_t samplephoton_idx = sbt->addCallableEntry(samplephoton_prog_group, _mesh_emitter_data.get());
+
+    EmitterVPtrTable table;
+    table.flags                 = _flags;
+    table.sampleCallIndex       = sample_idx;
+    table.evalCallIndex         = eval_idx;
+    table.evalPdfCallIndex      = eval_pdf_idx;
+    table.samplePhotonCallIndex = samplephoton_idx;
+
+    _vptr_table.upload(&table);
+
+    markInitialized();
+}
+}    // namespace atcg
